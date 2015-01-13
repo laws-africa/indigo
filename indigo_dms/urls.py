8a0a211c4f569902d41ce1a34ef62f6af40ab983
Greg Kempe <greg@kempe.net>
2015-01-13 10:02:41 +0200

    Basic document detail page.

--- indigo_dms/urls.py
+++ indigo_dms/urls.py
@@ -4,4 +4,5 @@ from indigo_dms import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^app/document/(?P<doc_id>\d+)/$', views.document, name='document'),
 )
--- indigo_dms/views.py
+++ indigo_dms/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Document
 
 def index(request):
     return render(request, 'index.html')
+
+def document(request, doc_id):
+    doc = get_object_or_404(Document, pk=doc_id)
+    return render(request, 'document/show.html', {
+        'doc': doc,
+        })
+    

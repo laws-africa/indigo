722fc75133574e9bba933062bedb85ef2c37e330
Greg Kempe <greg@kempe.net>
2015-01-15 17:04:17 +0200

    Redirect / to /library

--- indigo_app/urls.py
+++ indigo_app/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url, include
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', RedirectView.as_view(url='library', permanent=True)),
 
     url(r'^documents/(?P<doc_id>\d+)/$', views.document, name='document'),
     url(r'^library/$', views.library, name='library'),
--- indigo_app/views.py
+++ indigo_app/views.py
@@ -3,9 +3,6 @@ from django.shortcuts import render, get_object_or_404
 from indigo_api.models import Document
 from .forms import DocumentForm
 
-def index(request):
-    return render(request, 'index.html')
-
 def document(request, doc_id):
     doc = get_object_or_404(Document, pk=doc_id)
     form = DocumentForm(instance=doc)

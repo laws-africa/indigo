c1a69303215b8270fc77fbf6efae129f8c125c4a
Greg Kempe <greg@kempe.net>
2015-02-02 13:42:55 +0200

    Rest authentication

--- indigo/settings.py
+++ indigo/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'django_assets',
+
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+
     'django_extensions',
 
     # the Indigo API
@@ -136,8 +140,10 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # REST
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.SessionAuthentication',
+       'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
         'rest_framework.permissions.AllowAny',

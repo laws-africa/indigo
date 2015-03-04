a6737ef4f9d391a4edde60a497aac8206ba38d8c
Greg Kempe <greg@kempe.net>
2015-03-04 13:22:18 +0200

    Call out to slaw

--- indigo_api/importer.py
+++ indigo_api/importer.py
@@ -1,17 +1,28 @@
+import subprocess
+import tempfile
+import shutil
+import logging
+
+from .models import Document
+
 class Importer(object):
-    def import_from_upload(upload):
+    log = logging.getLogger(__name__)
+
+    def import_from_upload(self, upload):
         """ Create a new Document by importing it from a
         :class:`django.core.files.uploadedfile.UploadedFile` instance.
         """
         # we got a file
         if upload.content_type == 'application/pdf':
-            document = Document()
-            # TODO: do import
-            document.title = "Imported from %s" % upload.name
+            with self.tempfile_for_upload(upload) as f:
+                doc = self.import_from_pdf(f.name)
+
+            if not doc.title:
+                doc.title = "Imported from %s" % upload.name
 
         elif upload.content_type in ['text/xml', 'application/xml']:
-            document = Document()
-            document.content = upload.read().decode('utf-8')
+            doc = Document()
+            doc.content = upload.read().decode('utf-8')
 
         else:
             # bad type of file
@@ -20,4 +31,36 @@ class Importer(object):
         # TODO: handle doc input
         # TODO: handle plain text input
 
-        return document
+        return doc
+
+    def import_from_pdf(self, pdf):
+        # TODO:
+        # TODO: handle errors
+        cmd = 'slaw convert --output xml --input pdf'.split(' ') + [pdf]
+        self.log.info("Running %s" % cmd)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+
+        self.log.info("Subprocess exit code: %s" % p.returncode)
+        if p.returncode > 0:
+            raise ValueError("Error converting file: %s" % stderr)
+
+        doc = Document()
+        doc.content = stdout
+
+        self.log.info("Successfully imported")
+        return doc
+
+    def tempfile_for_upload(self, upload):
+        """ Uploaded files might not be on disk. If not, create temporary file. """
+        if hasattr(upload, 'temporary_file_path'):
+            return upload
+
+        f = tempfile.NamedTemporaryFile()
+
+        self.log.info("Copying uploaded file %s to temp file %s" % (upload, f.name))
+        shutil.copyfileobj(upload, f)
+        f.flush()
+        f.seek(0)
+
+        return f

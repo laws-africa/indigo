706683188a048b7d81ae4857b61099c8f6075c8d
Greg Kempe <greg@kempe.net>
2015-05-12 17:27:11 +0200

    Set site header in admin area

--- indigo/urls.py
+++ indigo/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+admin.site.site_header = 'Indigo Admin'
+
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('indigo_api.urls')),

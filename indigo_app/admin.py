d3131663a0d13ac25c16ac837f9bcc06014c451c
Greg Kempe <greg@kempe.net>
2015-05-21 10:06:40 +0200

    Move subtype back into api, since will be useful in templates

--- indigo_app/admin.py
+++ indigo_app/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Language, Subtype, Country
+from .models import Language, Country
 
 # Register your models here.
 admin.site.register(Language)
-admin.site.register(Subtype)
 admin.site.register(Country)

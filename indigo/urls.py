a904fd119e4244b2f528f5790ae6ab4b4d0fbe89
Greg Kempe <greg@kempe.net>
2015-08-21 14:17:00 +0200

    Robots.txt

--- indigo/urls.py
+++ indigo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.site.site_header = 'Indigo Admin'
 
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^api/', include('indigo_api.urls')),
 
     url(r'^', include('indigo_app.urls')),
+    url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 )

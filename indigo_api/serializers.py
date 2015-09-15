ea7f47405141b5572388a9b1688839eec9bc7615
Greg Kempe <greg@kempe.net>
2015-09-15 11:25:24 +0200

    Pagination

--- indigo_api/serializers.py
+++ indigo_api/serializers.py
@@ -387,28 +387,40 @@ class AtomFeed(feedgenerator.Atom1Feed):
     full_feed_title = "Indigo Full Document Feed"
     summary_feed_title = "Indigo Summary Feed"
 
-    def __init__(self, serializer=None, summary=True, *args, **kwargs):
+    def __init__(self, serializer=None, summary=True, next_url=None, previous_url=None, *args, **kwargs):
         self.serializer = serializer
         self.summary = summary
+
+        # pagination
+        self.next_url = next_url
+        self.previous_url = previous_url
+
         super(AtomFeed, self).__init__(*args, **kwargs)
 
     def add_root_elements(self, handler):
         super(AtomFeed, self).add_root_elements(handler)
 
+        # alternate full / summary feeds
         if self.summary:
             handler.addQuickElement("link", "", {
                 "rel": "alternate",
                 "title": self.full_feed_title,
                 "type": "application/atom+xml",
-                "href": self.feed['feed_url'].replace('feed.atom', 'full.atom'),
+                "href": self.feed['link'] + 'full.atom',
             })
         else:
             handler.addQuickElement("link", "", {
                 "rel": "alternate",
                 "title": self.summary_feed_title,
                 "type": "application/atom+xml",
-                "href": self.feed['feed_url'].replace('full.atom', 'feed.atom'),
+                "href": self.feed['link'] + 'feed.atom',
             })
+
+        # pagination
+        if self.next_url:
+            handler.addQuickElement("link", "", {"rel": "next", "href": self.next_url})
+        if self.previous_url:
+            handler.addQuickElement("link", "", {"rel": "previous", "href": self.previous_url})
 
     def add_item_elements(self, handler, item):
         from indigo_api.views import document_to_html
@@ -443,7 +455,7 @@ class AtomRenderer(XMLRenderer):
     # override the serializer class, we want a Document object
     serializer_class = NoopSerializer
 
-    def render(self, docs, media_type=None, renderer_context=None):
+    def render(self, data, media_type=None, renderer_context=None):
         self.serializer = DocumentSerializer(context=renderer_context)
         frbr_uri = renderer_context['kwargs']['frbr_uri']
 
@@ -454,17 +466,17 @@ class AtomRenderer(XMLRenderer):
         url = reverse('published-document-detail', request=renderer_context['request'],
                       kwargs={'frbr_uri': frbr_uri[1:]})
 
-        # TODO: pagination info
-
         feed = AtomFeed(
             title="%s - %s" % (title, frbr_uri),
             feed_url=renderer_context['request'].build_absolute_uri(),
             link=url,
             description="Indigo documents under the %s FRBR URI" % frbr_uri,
             serializer=self.serializer,
-            summary=summary)
+            summary=summary,
+            next_url=data['next'],
+            previous_url=data['previous'])
 
-        for doc in docs:
+        for doc in data['results']:
             self.add_item(feed, doc)
 
         return feed.writeString('utf-8')

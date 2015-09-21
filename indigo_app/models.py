c6da900ae0ed56b8d0f09a2c9225c27af6fe9f0f
Greg Kempe <greg@kempe.net>
2015-09-21 12:47:11 +0200

    FIX for country defaults for unknown countries.

--- indigo_app/models.py
+++ indigo_app/models.py
@@ -70,6 +70,9 @@ def update_user_country(sender, **kwargs):
     document = kwargs["instance"]
     user = document.updated_by_user
 
-    if not user.editor.country and document.country:
-        user.editor.country_code = document.country
-        user.editor.save()
+    if user and user.editor and not user.editor.country and document.country:
+        try:
+            user.editor.country_code = document.country
+            user.editor.save()
+        except Country.DoesNotExist:
+            pass

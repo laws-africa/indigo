83e44b90f574d18c49919268a3c89f4ed6a0200e
Greg Kempe <greg@kempe.net>
2017-03-17 08:19:39 +0200

    Update django storages, boto

--- indigo/settings.py
+++ indigo/settings.py
@@ -140,14 +140,14 @@ TEMPLATES = [
 
 # attachments
 if not DEBUG:
-    DEFAULT_FILE_STORAGE = 'indigo.botopatch.S3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_S3_FILE_OVERWRITE = False
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_S3_BUCKET')
-    AWS_S3_HOST = os.environ.get("AWS_S3_HOST", "s3-eu-west-1.amazonaws.com")
-    AWS_HEADERS = {
-        'Cache-Control': 'max-age=86400',
+    AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_HOST", "http://s3-eu-west-1.amazonaws.com")
+    AWS_S3_OBJECT_PARAMETERS = {
+        'CacheControl': 'max-age=86400',
     }
 
 

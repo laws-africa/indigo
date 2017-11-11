8c7c4e3d5a4e4073e82101cfc16bc78a1bd92739
Greg Kempe <greg@kempe.net>
2017-11-11 11:36:39 +0200

    Translate chapter

--- indigo_app/apps.py
+++ indigo_app/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class IndigoAppConfig(AppConfig):
     name = 'indigo_app'
     verbose_name = 'Indigo Editor'
+
+
+# Ensure that these translations are included by makemessages
+_('Chapter')
+_('Act')
+_('Government Notice')
+_('By-law')

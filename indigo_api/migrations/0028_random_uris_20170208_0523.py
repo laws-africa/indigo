508dc523a276f6496af512800eb4bbc86c75e418
Greg Kempe <greg@kempe.net>
2018-01-14 16:49:25 +0200

    Fix historical migrations so that the use the versioned model

--- indigo_api/migrations/0028_random_uris_20170208_0523.py
+++ indigo_api/migrations/0028_random_uris_20170208_0523.py
@@ -3,15 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-from indigo_api.models import random_frbr_uri, Document
+from indigo_api.models import random_frbr_uri
+from cobalt.act import Act
 
 
 def randomise_uris(apps, schema_editor):
     # randomise the FRBR URIs that are the old-style generic "/za/act/1980/01" URIs.
+    Document = apps.get_model("indigo_api", "Document")
     for doc in Document.objects.filter(frbr_uri='/za/act/1980/01'):
         uri = random_frbr_uri()
         uri.date = str(doc.created_at.year)
+
+        act = Act(doc.document_xml)
+        act.frbr_uri = uri.work_uri(False)
         doc.frbr_uri = uri.work_uri(False)
+        doc.document_xml = act.to_xml()
         doc.save()
 
 

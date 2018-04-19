f344ad1991bf8c87a69d534776a3fd017c150a69
Greg Kempe <greg@kempe.net>
2018-04-19 12:19:52 +0200

    Boto settings

--- indigo/settings.py
+++ indigo/settings.py
@@ -146,7 +146,6 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_S3_BUCKET')
-    AWS_S3_ENDPOINT_URL = os.environ.get("AWS_S3_HOST", "http://s3-eu-west-1.amazonaws.com")
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',

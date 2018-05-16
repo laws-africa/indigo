78250777f1022af3bdcac21472f90dabcd4a8c8f
Jan Zankowski <jan.zankowski+signup@gmail.com>
2018-05-16 19:48:41 +0300

    Polish text reformat: 1. de-hyphenate words, 2. remove linebreaks except for section boundaries.

--- indigo_api/importers/pl.py
+++ indigo_api/importers/pl.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from indigo_api.importers.base import Importer
 from indigo_api.importers.registry import importers
-
+import re
 
 @importers.register
 class ImporterPL(Importer):
@@ -11,5 +12,17 @@ class ImporterPL(Importer):
     slaw_grammar = 'pl'
 
     def reformat_text(self, text):
-        # TODO: do something intelligent
+        # Join hyphenated words - ones that have been split in middle b/c of line end.   
+        text = re.sub(ur"([a-ząćęłńśóźż])-\n([a-ząćęłńśóźż])", "\g<1>\g<2>", text)
+        # Remove all line breaks, except when the new line starts with a symbol known
+        # to be the start of a division.
+        text = re.sub(ur"\n(?!("
+                      u"DZIAŁ [IVXLC]|"
+                      u"Rozdział [IVXLC1-9]|"
+                      u"Art\.|"
+                      u"§ \d+[a-z]*\.|"
+                      u"\d+[a-z]*\.|"
+                      u"\d+[a-z]*\)|"
+                      u"[a-z]+\)|"
+                      u"–))", " ", text)
         return text

a4e035369c3112417b5d37c5b82a7e547683ced1
Greg Kempe <greg@kempe.net>
2018-05-22 12:51:48 +0100

    Auto-minimize sidebar
    FIX width bug

--- indigo_app/static/javascript/indigo/views/sidebar.js
+++ indigo_app/static/javascript/indigo/views/sidebar.js
@@ -4,12 +4,6 @@
   var $link = $('.sidebar .sidebar-minimizer');
 
   $link.on('click', function() {
-    if (document.body.classList.toggle('sidebar-minimized')) {
-      // now minimized
-      $link.find('i').removeClass('fa-chevron-left').addClass('fa-chevron-right');
-    } else {
-      // now expanded
-      $link.find('i').addClass('fa-chevron-left').removeClass('fa-chevron-right');
-    }
+    document.body.classList.toggle('sidebar-minimized');
   });
 })(window);

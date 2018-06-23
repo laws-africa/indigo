54b93c55ef7d69f55a9947b2d02e3d7b010b9652
Greg Kempe <greg@kempe.net>
2018-06-23 16:05:15 +0200

    Separate task detail and instruction views

--- indigo_workflow/views.py
+++ indigo_workflow/views.py
@@ -1,25 +1,9 @@
 import viewflow.flow.views as views
 
 
-class TaskDetailTemplateMixin(object):
-    """ Use the task_detail.html template for this task.
-    """
-    def get_template_names(self):
-        if self.template_name is None:
-            flow_task = self.activation.flow_task
-            opts = self.activation.flow_task.flow_class._meta
-
-            return (
-                '{}/{}/{}_detail.html'.format(opts.app_label, opts.flow_label, flow_task.name),
-                '{}/{}/task_detail.html'.format(opts.app_label, opts.flow_label),
-                'viewflow/flow/task_detail.html')
-        else:
-            return [self.template_name]
-
-
-class HumanInteractionView(TaskDetailTemplateMixin, views.UpdateProcessView):
+class HumanInteractionView(views.UpdateProcessView):
     pass
 
 
-class ReviewTaskView(TaskDetailTemplateMixin, views.UpdateProcessView):
+class ReviewTaskView(views.UpdateProcessView):
     pass

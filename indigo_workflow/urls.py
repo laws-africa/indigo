6af695789ca2852ae689f4ce720d407918b1aa27
Greg Kempe <greg@kempe.net>
2018-06-24 17:32:55 +0200

    List of tasks

--- indigo_workflow/urls.py
+++ indigo_workflow/urls.py
@@ -2,8 +2,16 @@ from django.conf.urls import url, include
 from viewflow.flow.viewset import FlowViewSet
 
 from .flows import ListWorksFlow
+import indigo_workflow.views as views
 
+
+list_works_set = FlowViewSet(ListWorksFlow)
+list_works_set.process_list_view = None
+list_works_set.inbox_list_view = None
+list_works_set.queue_list_view = None
+list_works_set.archive_list_view = None
 
 urlpatterns = [
-    url(r'^listworks/', include(FlowViewSet(ListWorksFlow).urls), name='list-works'),
+    url(r'^listworks/', include(list_works_set.urls), name='list-works'),
+    url(r'^tasks/', views.TaskListView.as_view(), name='all-task-list'),
 ]
--- indigo_workflow/views.py
+++ indigo_workflow/views.py
@@ -1,4 +1,7 @@
+from django.views.generic import TemplateView
+
 import viewflow.flow.views as views
+from viewflow.models import Task
 
 
 class HumanInteractionView(views.UpdateProcessView):
@@ -7,3 +10,20 @@ class HumanInteractionView(views.UpdateProcessView):
 
 class ReviewTaskView(views.UpdateProcessView):
     pass
+
+
+class TaskListView(TemplateView):
+    template_name = 'indigo_workflow/task_list.html'
+
+    def __init__(self, *args, **kwargs):
+        super(TaskListView, self).__init__(*args, **kwargs)
+
+        from indigo_workflow.flows import ListWorksFlow
+        self.flows = [ListWorksFlow]
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(TaskListView, self).get_context_data(**kwargs)
+        kwargs['assigned_tasks'] = Task.objects.inbox(self.flows, self.request.user).order_by('-created')
+        kwargs['available_tasks'] = Task.objects.queue(self.flows, self.request.user).order_by('-created')
+
+        return kwargs

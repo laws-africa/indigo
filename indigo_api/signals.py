09f1e249e42eafa8c00fbd236b6e6dea49bde740
Greg Kempe <greg@kempe.net>
2018-07-08 15:48:54 +0200

    Fire a signal when a work is created or changed

--- indigo_api/signals.py
+++ indigo_api/signals.py
@@ -1,6 +1,11 @@
 from django.dispatch import Signal
 
 
+work_changed = Signal(providing_args=["work", "request"])
+""" A user has created or made changes to a work.
+"""
+
+
 document_published = Signal(providing_args=["document", "request"])
-""" A user has changed a from draft to published.
+""" A user has changed a document from draft to published.
 """

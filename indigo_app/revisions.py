11e20fb2ae999746d1a0ada56769f4a29eb76a34
Greg Kempe <greg@kempe.net>
2018-07-15 17:15:37 +0400

    Missing file

---
+++ indigo_app/revisions.py
@@ -0,0 +1,27 @@
+from itertools import izip
+
+import jsonpatch
+
+
+def decorate_versions(versions, ignore=('updated_at', 'updated_by_user')):
+    # make pretty differences
+    for curr, prev in izip(versions, list(versions[1:]) + [None]):
+        curr_d = curr.field_dict
+        prev_d = {} if prev is None else prev.field_dict
+
+        for fld in ignore:
+            for d in [curr_d, prev_d]:
+                if fld in d:
+                    del d[fld]
+
+        patch = jsonpatch.make_patch(curr_d, prev_d)
+
+        curr.previous = prev
+        curr.changes = sorted([{
+            'field': p['path'][1:].replace('_', ' '),
+            'path': p['path'],
+            'old': prev_d.get(p['path'][1:]),
+            'new': curr_d.get(p['path'][1:]),
+        } for p in patch], key=lambda x: x['field'])
+
+    return versions

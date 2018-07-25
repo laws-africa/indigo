55774b1fc640fe1ac4457b8d91176bd3acd48366
Greg Kempe <greg@kempe.net>
2018-07-25 16:59:15 +0200

    Process form for batch works

--- indigo_app/forms.py
+++ indigo_app/forms.py
@@ -1,8 +1,20 @@
 from django.forms import ModelForm
+from django.core.validators import URLValidator
+from django import forms
 
 from indigo_api.models import Document
+
 
 class DocumentForm(ModelForm):
     class Meta:
         model = Document
         exclude = ('document_xml', 'created_at', 'updated_at', 'created_by_user', 'updated_by_user',)
+
+
+class BatchCreateWorkForm(forms.Form):
+    spreadsheet_url = forms.URLField(required=True, validators=[
+        URLValidator(
+            schemes=['https'],
+            regex='^https://docs.google.com/spreadsheets/d/\w+/',
+            message="Please enter a valid Google Sheets URL, such as https://docs.google.com/spreadsheets/d/ABCXXX/", code='bad')
+    ])

93eef33192d111c5bd1df77e676778d7b01a3a19
goose-life <32566441+goose-life@users.noreply.github.com>
2018-08-22 09:54:16 +0200

    improve Updated column

--- indigo_app/static/javascript/indigo/views/library.js
+++ indigo_app/static/javascript/indigo/views/library.js
@@ -380,10 +380,16 @@
         work.work_docs = work_docs;
 
         // get most recent update by sorting docs for each work by updated_at, most recent first -- need to get string instead of object Object
-        work.most_recent_update = work_docs.sort(function(a, b) {
+        work.most_recent_doc = work_docs.sort(function(a, b) {
           var updated_dates = -a.get('updated_at').localeCompare(b.get('updated_at'));
           return updated_dates;
         })[0];
+
+        if (work.most_recent_doc) {
+          work.most_recent_doc = work.most_recent_doc.toJSON();
+        }
+
+
 
 
         return work;

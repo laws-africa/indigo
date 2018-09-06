8a4b12e9fb2bc21d11bf8047b5166f3404a9e0ec
Greg Kempe <greg@kempe.net>
2018-09-06 13:51:38 +0200

    Tests for AFR and ENG constitution finder

--- indigo_za/refs.py
+++ indigo_za/refs.py
@@ -20,9 +20,11 @@ class RefsFinderENG(BaseRefsFinder):
     act_re = re.compile(r'\bAct,?\s+([nN]o\.?\s*)?(\d+)+\s+of\s+(\d{4})|\bConstitution\b(\s+of(\s+the\s+Republic\s+of)?\s+South\s+Africa)?((\s+Act)?,?\s+1996)?')
     candidate_xpath = ".//text()[(contains(., 'Act') or contains(., 'Constitution')) and not(ancestor::a:ref)]"
 
-
     def make_href(self, match):
         if 'Constitution' in match.group(0):
+            return '/za/act/1996/constitution'
+        elif self.frbr_uri.country == 'za' and match.group(3) == '1996' and match.group(2) == '108':
+            # Act 108 of 1996 was originally the constitution
             return '/za/act/1996/constitution'
         else:
             return '/%s/act/%s/%s' % (self.frbr_uri.country, match.group(3), match.group(2))
@@ -41,11 +43,14 @@ class RefsFinderAFR(BaseRefsFinder):
     # country, language, locality
     locale = (None, 'afr', None)
 
-    act_re = re.compile(r'\bWet,?\s+([nN]o\.?\s*)?(\d+)+\s+van\s+(\d{4})|\bGrondwet\b(\s+van(\s+die\s+Republiek\s+van)?\s+Suid-Afrika)?((\s+Wet)?,?\s+1996)?')
+    act_re = re.compile(r'\bWet,?\s+([nN]o\.?\s*)?(\d+)+\s+van\s+(\d{4})|\bGrondwet\b(\s+van(\s+die\s+Republiek\s+van)?\s+Suid[- ]Afrika)?((\s+Wet)?,?\s+1996)?')
     candidate_xpath = ".//text()[(contains(., 'Wet') or contains(., 'Grondwet')) and not(ancestor::a:ref)]"
 
     def make_href(self, match):
         if 'Grondwet' in match.group(0):
+            return '/za/act/1996/constitution'
+        elif self.frbr_uri.country == 'za' and match.group(3) == '1996' and match.group(2) == '108':
+            # Act 108 of 1996 was originally the constitution
             return '/za/act/1996/constitution'
         else:
             return '/%s/act/%s/%s' % (self.frbr_uri.country, match.group(3), match.group(2))

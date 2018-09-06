ab01edc58dbf2a38f26dcd7744e471c585841c25
goose-life <32566441+goose-life@users.noreply.github.com>
2018-09-06 21:28:04 +0200

    add userprofile model (still needs recent activity, faves, badges)

--- indigo_social/models.py
+++ indigo_social/models.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=1024, help_text="A short bio")
+    qualifications = models.TextField(max_length=512, help_text="Qualifications")
+    skills = models.CharField(max_length=256, help_text="Skills")
+    organisations = models.CharField(max_length=256, help_text="Organisation(s)")
+    specialisations = models.CharField(max_length=256, help_text="Specialisation(s)")
+    areas_of_law = models.CharField(max_length=256, help_text="Area(s) of law")
+    profile_photo = models.ImageField()
+    twitter_profile = models.URLField()
+    linkedin_profile = models.URLField()
+

346c717e77317d265276be8966956c98930dbb53
Greg Kempe <greg@kempe.net>
2018-10-05 15:49:24 +0200

    User can choose country via indigo_app

--- indigo_app/views/users.py
+++ indigo_app/views/users.py
@@ -1,27 +1,34 @@
 from django.conf import settings
-from django.views.generic import DetailView, FormView, UpdateView
+from django.views.generic import DetailView, UpdateView
 from django.urls import reverse
 from allauth.utils import get_request_param
 
-from indigo_app.forms import UserForm
+from indigo_app.forms import UserEditorForm
+from indigo_app.models import Editor
 from .base import AbstractAuthedIndigoView
 
 
-class EditAccountView(AbstractAuthedIndigoView, FormView):
+class EditAccountView(AbstractAuthedIndigoView, UpdateView):
+    model = Editor
     template_name = 'indigo_app/user_account/edit.html'
-    form_class = UserForm
+    form_class = UserEditorForm
 
     def get_success_url(self):
         return reverse('edit_account')
 
     def get_form_kwargs(self):
         kwargs = super(EditAccountView, self).get_form_kwargs()
-        kwargs['instance'] = self.request.user
+        kwargs['instance'] = self.request.user.editor
         return kwargs
 
-    def form_valid(self, form):
-        self.object = form.save()
-        return super(EditAccountView, self).form_valid(form)
+    def get_object(self, queryset=None):
+        return self.request.user.editor
+
+    def get_initial(self):
+        initial = super(EditAccountView, self).get_initial()
+        initial['first_name'] = self.request.user.first_name
+        initial['last_name'] = self.request.user.last_name
+        return initial
 
 
 class EditAccountAPIView(AbstractAuthedIndigoView, DetailView):

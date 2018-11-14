497b7b8cea44ca106091e7f7216444d302c5b4ce
goose-life <32566441+goose-life@users.noreply.github.com>
2018-11-14 14:18:08 +0200

    don't get rid of fill-in-the-blanks (increased from 5 to 10)

--- indigo_za/importer.py
+++ indigo_za/importer.py
@@ -30,7 +30,7 @@ class ImporterZA(Importer):
         r'^\s*page \d+( of \d+)?\s*\n',
         r'^\s*\d*\s*No\. \d+$',
         # get rid of lines with lots of ____ or ---- chars, they're usually pagebreaks
-        r'^.*[_-]{5}.*$',
+        r'^\s*[_-]{10}\s*$',
     ]), re.MULTILINE)
 
     def reformat_text(self, text):

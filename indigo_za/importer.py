a6dc778267cd3185ae7629aadf0408738dac7380
goose-life <32566441+goose-life@users.noreply.github.com>
2018-11-28 13:08:08 +0200

    remove entire lines of --- or ___ but not if there's text on a line with it

--- indigo_za/importer.py
+++ indigo_za/importer.py
@@ -30,7 +30,8 @@ class ImporterZA(Importer):
         r'^\s*page \d+( of \d+)?\s*\n',
         r'^\s*\d*\s*No\. \d+$',
         # get rid of lines with lots of ____ or ---- chars, they're usually pagebreaks
-        r'^\s*[_-]{5}\s*$',
+        # but not fill-in-the-blanks in forms
+        r'^\s*[_-]{5,}\s*$',
     ]), re.MULTILINE)
 
     def reformat_text(self, text):

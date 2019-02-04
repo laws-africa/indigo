2149cf726cecdfb6893237d8203e393e45890d11
Greg Kempe <greg@kempe.net>
2019-02-04 17:42:22 +0200

    Flag to support unversioned (latest-only) content API
    This makes it easier to be backwards compatible with AfricanLII
    
    This can be removed once we no longer need to support the old
    unversioned API

--- indigo_content_api/urls.py
+++ indigo_content_api/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
 import views
@@ -6,7 +7,13 @@ import views
 router = DefaultRouter(trailing_slash=False)
 router.register(r'countries', views.CountryViewSet, base_name='country')
 
-# Versioned API URLs
-urlpatterns = [
-    url(r'^v1/', include('indigo_content_api.urls_v1', namespace='v1'))
-]
+if settings.INDIGO_CONTENT_API_VERSIONED:
+    # Versioned API URLs
+    urlpatterns = [
+        url(r'^v1/', include('indigo_content_api.urls_v1', namespace='v1'))
+    ]
+else:
+    # Unversioned API URLs, latest only
+    urlpatterns = [
+        url(r'^', include('indigo_content_api.urls_v1', namespace='v1'))
+    ]

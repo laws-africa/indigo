f287a0b0547ee51d2badc08fe1e881e30e642ebf
AliciadW <alicia.dewet@living-stratus.com>
2019-03-06 15:11:15 +0200

    Fixes as per review comments

--- indigo_app/static/javascript/indigo/utils.js
+++ indigo_app/static/javascript/indigo/utils.js
@@ -19,13 +19,15 @@ $(function() {
 // Show popover when hovering on selected links
 $('body').on('mouseenter', 'a[data-popup-url]', function() {
   var _this = this;
-  $.get(this.href + 'popup').then(function(html) {
+  var popupUrl = $('a[data-popup-url]').attr('data-popup-url');
+
+  $.get(popupUrl).then(function(html) {
     $(_this).popover({content: html, html: true}).popover('show');
   });
   $('.popover').mouseleave(function () {
     $(_this).popover('hide');
   });
-}).on('mouseleave','a[href^="/works/"]:not(.btn)', function(){
+}).on('mouseleave','a[data-popup-url]', function(){
   var _this = this;
   setTimeout(function () {
     if (!$('.popover:hover').length) {

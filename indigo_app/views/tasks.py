b0fbdf40ff1eaef45a9a01c9687bc9dd7b85cfc5
goose-life <32566441+goose-life@users.noreply.github.com>
2019-04-01 13:55:23 +0200

    unassign tasks on cancellation
    partially closes #443

--- indigo_app/views/tasks.py
+++ indigo_app/views/tasks.py
@@ -266,7 +266,7 @@ class TaskChangeStateView(TaskViewBase, View, SingleObjectMixin):
                         messages.success(request, u"Task '%s' has been unsubmitted and reassigned" % task.title)
 
                 else:
-                    if verb == 'closed':
+                    if verb == 'closed' or verb == 'cancelled':
                         task.assigned_to = None
                     action.send(user, verb=verb, action_object=task,
                                 place_code=task.place.place_code)

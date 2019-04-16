4b0bff44917d0765379835d1068606b9ab4979a3
goose-life <32566441+goose-life@users.noreply.github.com>
2019-04-16 13:48:28 +0200

    add conditions to permissions to submit, unsubmit, and close tasks

--- indigo_api/models.py
+++ indigo_api/models.py
@@ -1103,7 +1103,9 @@ class Task(models.Model):
     # submit for review
     def may_submit(self, view):
         return view.request.user.is_authenticated and \
-            view.request.user.editor.has_country_permission(view.country) and view.request.user.has_perm('indigo_api.submit_task')
+            view.request.user.editor.has_country_permission(view.country) and \
+            view.request.user.has_perm('indigo_api.submit_task') and \
+            view.request.user == self.assigned_to
 
     @transition(field=state, source=['open'], target='pending_review', permission=may_submit)
     def submit(self, user):
@@ -1130,7 +1132,9 @@ class Task(models.Model):
     # unsubmit – moves back to 'open'
     def may_unsubmit(self, view):
         return view.request.user.is_authenticated and \
-            view.request.user.editor.has_country_permission(view.country) and view.request.user.has_perm('indigo_api.unsubmit_task')
+        view.request.user.editor.has_country_permission(view.country) and \
+        view.request.user.has_perm('indigo_api.unsubmit_task') and \
+        view.request.user != self.last_assigned_to
 
     @transition(field=state, source=['pending_review'], target='open', permission=may_unsubmit)
     def unsubmit(self, user):
@@ -1139,7 +1143,9 @@ class Task(models.Model):
     # close
     def may_close(self, view):
         return view.request.user.is_authenticated and \
-            view.request.user.editor.has_country_permission(view.country) and view.request.user.has_perm('indigo_api.close_task')
+        view.request.user.editor.has_country_permission(view.country) and  \
+        view.request.user.has_perm('indigo_api.close_task') and \
+        view.request.user != self.last_assigned_to
 
     @transition(field=state, source=['pending_review'], target='done', permission=may_close)
     def close(self, user):

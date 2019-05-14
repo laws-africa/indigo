3f0d40f9185532f2e6c2812422cf9321cb0cf7db
AliciadW <alicia.dewet@living-stratus.com>
2019-05-14 09:54:37 +0200

    Test fix for popover not closing all the time

--- indigo_app/static/javascript/indigo/utils.js
+++ indigo_app/static/javascript/indigo/utils.js
@@ -54,7 +54,7 @@ $('body').on('mouseenter', 'a[data-popup-url]', function() {
   $('.popover').mouseleave(function () {
     $(_this).popover('hide');
   });
-}).on('mouseleave','a[data-popup-url]', function(){
+}).on('mouseleave','a[data-popup-url], .popover', function(){
   var _this = this;
   setTimeout(function () {
     if (!$('.popover:hover').length) {
@@ -62,5 +62,3 @@ $('body').on('mouseenter', 'a[data-popup-url]', function() {
     }
   }, 300);
 });
-
-

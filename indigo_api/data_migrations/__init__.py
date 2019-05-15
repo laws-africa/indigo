2861c8918242c4b8e990cd90aa8feb6408505982
Greg Kempe <greg@kempe.net>
2019-05-15 15:49:02 +0200

    Data migration for schedules

---
+++ indigo_api/data_migrations/__init__.py
@@ -0,0 +1,49 @@
+
+
+class AKNMigration(object):
+    def migrate_document(self, document):
+        return self.migrate_act(document.doc)
+
+
+class ScheduleArticleToHcontainer(AKNMigration):
+    """ Change from using article as the main schedule container
+    to using hcontainer.
+
+    Migrates:
+
+        <mainBody>
+          <article id="schedule2">
+            <heading>BY-LAWS REPEALED BY SECTION 99</heading>
+            <paragraph id="schedule2.paragraph-0">
+
+    to:
+
+        <mainBody>
+          <hcontainer id="schedule2" name="schedule">
+            <heading>Schedule 2</heading>
+            <subeading>BY-LAWS REPEALED BY SECTION 99</subeading>
+            <paragraph id="schedule2.paragraph-0">
+    """
+    def migrate_act(self, act):
+        changed = False
+
+        for article in act.root.xpath('//a:mainBody/a:article', namespaces={'a': act.namespace}):
+            changed = True
+
+            # change to hcontainer
+            article.tag = '{%s}hcontainer' % act.namespace
+            article.set('name', 'schedule')
+
+            # make heading a subheading
+            try:
+                heading = article.heading
+                heading.tag = '{%s}subheading' % act.namespace
+            except AttributeError:
+                pass
+
+            # add a new heading
+            alias = article.getparent().getparent().meta.identification.FRBRWork.FRBRalias.get('value')
+            heading = act._maker.heading(alias)
+            article.insert(0, heading)
+
+        return changed

08eb966a340f41535087d16bc2508fd24c8104b6
Greg Kempe <greg@kempe.net>
2019-05-17 19:31:04 +0200

    Task approved email

--- indigo_app/notifications.py
+++ indigo_app/notifications.py
@@ -9,15 +9,22 @@ from indigo_api.models import Task
 
 class Notifier(object):
     def send_task_notifications(self, action):
-        if action.verb in ['assigned']:
+        task = action.action_object
 
-            self.send_templated_email('task_assigned', [action.target.email],
-                {
+        if action.verb == 'assigned':
+            self.send_templated_email('task_assigned', [action.target.email], {
                     'action': action,
-                    'task': action.action_object,
+                    'task': task,
                     'recipient': action.target,
-                },
-            )
+                })
+
+        elif action.verb == 'closed':
+            if task.last_assigned_to and task.last_assigned_to != action.actor:
+                self.send_templated_email('task_closed_submitter', [task.last_assigned_to.email], {
+                        'action': action,
+                        'task': action.action_object,
+                        'recipient': task.last_assigned_to,
+                    })
 
     def send_templated_email(self, template_name, recipient_list, context, **kwargs):
         real_context = {

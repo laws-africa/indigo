3511872313f3ef6ef907236436c639c9f485f59d
Greg Kempe <greg@kempe.net>
2019-06-02 16:52:30 +0200

    FIX for django-webtest 1.9.5

--- indigo/settings.py
+++ indigo/settings.py
@@ -277,9 +277,9 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 # REST
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-    ),
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],

b9b3e06e657ef4e1f8b845c0f13ab546333e74fd
Greg Kempe <greg@kempe.net>
2019-06-02 12:07:17 +0200

    Link to publication. See #593

--- indigo_content_api/tests/test_content_api.py
+++ indigo_content_api/tests/test_content_api.py
@@ -428,7 +428,7 @@ class PublishedAPITest(APITestCase):
         response = self.client.get('/api/v1/za/act/2014/10/eng@2014-02-12.json')
         assert_equal(response.status_code, 200)
 
-        assert_equal(response.data['publication_document']['url'], 'http://testserver/api/v1/za/act/2014/10/eng@2014-02-12/media/za-act-2014-10-publication-document.pdf')
+        assert_equal(response.data['publication_document']['url'], 'http://localhost:8000/works/za/act/2014/10/media/publication/za-act-2014-10-publication-document.pdf')
 
     def test_published_search_perms(self):
         self.client.logout()

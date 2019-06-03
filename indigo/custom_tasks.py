c3514a461c5aec095442b70184ca5c7948f1765b
Greg Kempe <greg@kempe.net>
2019-06-03 17:01:00 +0200

    Docs

--- indigo/custom_tasks.py
+++ indigo/custom_tasks.py
@@ -4,7 +4,14 @@ from indigo.plugins import LocaleBasedRegistry, LocaleBasedMatcher
 
 
 class CustomTask(LocaleBasedMatcher):
+    """ Parent class for providing custom functionality for Indigo tasks.
+
+    To create a custom task, create a subclass of CustomTask and register
+    it with the tasks registry.
+    """
+
     locale = (None, None, None)
+    """ Locale for this object, as a tuple: (country, language, locality). None matches anything."""
 
     def setup(self, task):
         self.task = task

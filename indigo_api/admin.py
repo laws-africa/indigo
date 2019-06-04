5e6021a7e7ca2e9738e433639249b3de1b460d3c
Greg Kempe <greg@kempe.net>
2019-06-04 13:59:58 +0200

    Tweaks and editing for taxonomies

--- indigo_api/admin.py
+++ indigo_api/admin.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
 
-from .models import Document, Subtype, Colophon, Work, TaskLabel, TaxonomyVocabulary
+from .models import Document, Subtype, Colophon, Work, TaskLabel, TaxonomyVocabulary, VocabularyTopic
 
 admin.site.register(Subtype)
 
@@ -41,10 +41,15 @@ class TaskLabelAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class VocabularyTopicInline(admin.TabularInline):
+    model = VocabularyTopic
+    can_delete = True
+    extra = 3
+
+
 @admin.register(TaxonomyVocabulary)
 class TaxonomyVocabularyAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'title', 'authority',)
+    list_display = ('title', 'authority', '__str__')
     prepopulated_fields = {"slug": ("authority", "name")}
-
-
-
+    inlines = (VocabularyTopicInline, )
+    fields = ('title', 'authority', 'name', 'slug')

41f6c4bc9eba29d30928d752335b9bf58552cc58
Greg Kempe <greg@kempe.net>
2019-06-26 22:23:14 +0200

    Copyright

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Indigo Platform'
-copyright = u'2015, Code for South Africa'
+copyright = u'2015-2019, AfricanLII'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -214,7 +214,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   ('index', 'IndigoPlatform.tex', u'Indigo Platform Documentation',
-   u'Code for South Africa', 'manual'),
+   u'AfricanLII', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -244,7 +244,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'indigoplatform', u'Indigo Platform Documentation',
-     [u'Code for South Africa'], 1)
+     [u'AfricanLII'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -258,7 +258,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'IndigoPlatform', u'Indigo Platform Documentation',
-   u'Code for South Africa', 'IndigoPlatform', 'One line description of project.',
+   u'AfricanLII', 'IndigoPlatform', 'One line description of project.',
    'Miscellaneous'),
 ]
 

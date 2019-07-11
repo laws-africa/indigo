49a833801a4afe2fb5a5f48867be9111d382268f
Greg Kempe <greg@kempe.net>
2019-07-11 19:48:08 +0200

    Make WorkDetailZA za-specific

--- indigo_za/work_detail.py
+++ indigo_za/work_detail.py
@@ -4,6 +4,9 @@ from indigo.plugins import plugins
 
 @plugins.register('work-detail')
 class WorkDetailZA(BaseWorkDetail):
+    locale = ('za', None, None)
+    """ Locale for this object, as a tuple: (country, language, locality). None matches anything."""
+
     def work_numbered_title(self, work):
         uri = work.work_uri
 

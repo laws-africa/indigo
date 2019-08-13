4828508a8d2882116438e19c457f2b30542c84b3
Robley <robleyadrian@gmail.com>
2019-08-13 17:20:02 +0300

    move user signed up signal handler into signals.py from notifications.py

--- indigo_app/signals.py
+++ indigo_app/signals.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.models import User
 from django.db.models import signals
 from django.dispatch import receiver
 
+from allauth.account.signals import user_signed_up
 from actstream.models import Action
 from django_comments.models import Comment
 from django_comments.signals import comment_was_posted
 
 from indigo_api.models import Task
-from indigo_app.notifications import notify_task_action, notify_comment_posted
+from indigo_app.notifications import notify_task_action, notify_comment_posted, notify_new_user_signed_up
 
 
 @receiver(signals.post_save, sender=Action)
@@ -24,3 +26,11 @@ def post_comment_save_notification(sender, **kwargs):
     """
     if kwargs['comment']:
         notify_comment_posted(kwargs['comment'].pk)
+
+
+@receiver(user_signed_up, sender=User)
+def user_sign_up_signal_handler(**kwargs):
+    """ Send an email to the admins when a new user signs up
+    """
+    if kwargs['user']:
+        notify_new_user_signed_up(kwargs['user'])

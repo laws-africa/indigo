4724929d069757a7cac4a45933d29d3c4525743f
Greg Kempe <greg@kempe.net>
2019-08-22 12:58:42 +0200

    FIX busted scrolling in document revision view

---
+++ indigo_app/static/javascript/indigo/views/diff_navigator.js
@@ -0,0 +1,36 @@
+(function(exports) {
+  "use strict";
+
+  if (!exports.Indigo) exports.Indigo = {};
+  Indigo = exports.Indigo;
+
+  /**
+   * Handle the revisions of a document.
+   */
+  Indigo.DiffNavigator = Backbone.View.extend({
+    events: {
+      'click .prev-change-btn': 'prevChange',
+      'click .next-change-btn': 'nextChange',
+    },
+
+    initialize: function (options) {
+      this.changedElements = $(this.el.getAttribute('data-target')).find('ins, del, .ins, .del');
+      this.currentElementIndex = -1;
+    },
+
+    prevChange: function (e) {
+      // workaround for (x - 1) % 3 == -1
+      if (this.currentElementIndex <= 0) {
+        this.currentElementIndex = this.changedElements.length - 1;
+      } else {
+        this.currentElementIndex--;
+      }
+      this.changedElements[this.currentElementIndex].scrollIntoView();
+    },
+
+    nextChange: function (e) {
+      this.currentElementIndex = (this.currentElementIndex + 1) % this.changedElements.length;
+      this.changedElements[this.currentElementIndex].scrollIntoView();
+    }
+  });
+})(window);

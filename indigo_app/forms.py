e0972ad6c87caa61fc57fbd7f89fe7f339ce2b2f
Robley <robleyadrian@gmail.com>
2019-08-23 17:24:17 +0300

    revert list() changes

--- indigo_app/forms.py
+++ indigo_app/forms.py
@@ -93,7 +93,7 @@ class WorkPropertyForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(WorkPropertyForm, self).__init__(*args, **kwargs)
-        self.fields['key'].choices = WorkProperty.KEYS.items()
+        self.fields['key'].choices = list(WorkProperty.KEYS.items())
 
     def clean(self):
         super(WorkPropertyForm, self).clean()
@@ -106,13 +106,13 @@ class BaseWorkPropertyFormSet(BaseModelFormSet):
     def setup_extras(self):
         # add extra forms for the properties we don't have yet
         existing = set([p.key for p in self.queryset.all()])
-        missing = [key for key in WorkProperty.KEYS.keys() if key not in existing]
+        missing = [key for key in list(WorkProperty.KEYS.keys()) if key not in existing]
         self.extra = len(missing)
         self.initial_extra = [{'key': key} for key in missing]
 
     def keys_and_forms(self):
         # (value, label) pairs sorted by label
-        keys = sorted(WorkProperty.KEYS.items(), key=lambda x: x[1])
+        keys = sorted(list(WorkProperty.KEYS.items()), key=lambda x: x[1])
         forms_by_key = {f['key'].value(): f for f in self.forms}
         return [{
             'key': val,

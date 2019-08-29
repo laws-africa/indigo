3b2a530c711198419e4cc9729f289d7506f7e65c
Robley <robleyadrian@gmail.com>
2019-08-29 21:00:21 +0300

    add badge_awarde signal handler as sent from Pinax

--- indigo_app/signals.py
+++ indigo_app/signals.py
@@ -6,9 +6,10 @@ from allauth.account.signals import user_signed_up
 from actstream.models import Action
 from django_comments.models import Comment
 from django_comments.signals import comment_was_posted
+from pinax.badges.signals import badge_awarded
 
 from indigo_api.models import Task, Annotation
-from indigo_app.notifications import notify_task_action, notify_comment_posted, notify_new_user_signed_up, notify_annotation_reply_posted
+from indigo_app.notifications import notify_task_action, notify_comment_posted, notify_new_user_signed_up, notify_annotation_reply_posted, notify_user_badge_earned
 
 
 @receiver(signals.post_save, sender=Action)
@@ -40,3 +41,8 @@ def user_sign_up_signal_handler(**kwargs):
 def post_annotation_reply(sender, **kwargs):
     if kwargs['instance'].in_reply_to:
         notify_annotation_reply_posted(kwargs['instance'].pk)
+
+@receiver(badge_awarded)
+def post_badge_earned(sender, **kwargs):
+	if kwargs.get('badge_award') and not kwargs['badge_award'].can_award_manually:
+			notify_user_badge_earned(kwargs['badge_award'].pk)

b84d55fbf2b3a99517b22cc3292b3d1171ae542b
goose-life <32566441+goose-life@users.noreply.github.com>
2019-12-20 15:52:55 +0200

    move (simplified) now-passing test to under 'edge'

--- indigo/tests/test_refs.py
+++ indigo/tests/test_refs.py
@@ -16,264 +16,6 @@ class SectionRefsFinderTestCase(TestCase):
         self.eng = Language.for_code('eng')
         self.maxDiff = None
 
-    def test_section_latest(self):
-        document = Document(
-            document_xml=document_fixture(
-                xml="""
-      <section id="section-1">
-        <num>1.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-4">
-        <num>4.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-6">
-        <num>6.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-7">
-        <num>7.</num>
-        <heading>Active ref heading</heading>
-        <content>
-          <p>A person who contravenes sections 4(1) and (2), 6(3), 10(1) and (2), 11(1), 12(1), 19(1), 19(3), 20(1), 20(2), 21(1), 22(1), 24(1), 25(3), (4) , (5) and (6) , 26(1), (2), (3) and (5), 28(1), (2) and (3) is guilty of an offence.</p>
-        </content>
-      </section>
-      <section id="section-10">
-        <num>10.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-11">
-        <num>11.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-12">
-        <num>12.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-19">
-        <num>19.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-20">
-        <num>20.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-21">
-        <num>21.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-22">
-        <num>22.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-24">
-        <num>24.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-25">
-        <num>25.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-26">
-        <num>26.</num>
-        <heading>Important heading</heading>
-        <content>
-          <p>An important provision.</p>
-        </content>
-      </section>
-      <section id="section-26B">
-        <num>26B.</num>
-        <heading>Another important heading</heading>
-        <content>
-          <p>Another important provision.</p>
-        </content>
-      </section>
-      <section id="section-30">
-        <num>30.</num>
-        <heading>More important</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-31">
-        <num>31.</num>
-        <heading>More important</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-        """
-            ),
-            language=self.eng)
-
-        expected = Document(
-            document_xml=document_fixture(
-                xml="""
-      <section id="section-1">
-        <num>1.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-4">
-        <num>4.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-6">
-        <num>6.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-7">
-        <num>7.</num>
-        <heading>Active ref heading</heading>
-        <content>
-          <p>A person who contravenes sections <ref href="#section-4">4</ref>(1) and (2), <ref href="#section-6">6</ref>(3), <ref href="#section-10">10</ref>(1) and (2), <ref href="#section-11">11</ref>(1), <ref href="#section-12">12</ref>(1), <ref href="#section-19">19</ref>(1), <ref href="#section-19">19</ref>(3), <ref href="#section-20">20</ref>(1), <ref href="#section-20">20</ref>(2), <ref href="#section-21">21</ref>(1), <ref href="#section-22">22</ref>(1), <ref href="#section-24">24</ref>(1), <ref href="#section-25">25</ref>(3), (4) , (5) and (6) , <ref href="#section-26">26</ref>(1), (2), (3) and (5), <ref href="#section-28">28</ref>(1), (2) and (3) is guilty of an offence.</p>
-        </content>
-      </section>
-      <section id="section-10">
-        <num>10.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-11">
-        <num>11.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-12">
-        <num>12.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-19">
-        <num>19.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-20">
-        <num>20.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-21">
-        <num>21.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-22">
-        <num>22.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-24">
-        <num>24.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-25">
-        <num>25.</num>
-        <heading>Unimportant</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-26">
-        <num>26.</num>
-        <heading>Important heading</heading>
-        <content>
-          <p>An important provision.</p>
-        </content>
-      </section>
-      <section id="section-26B">
-        <num>26B.</num>
-        <heading>Another important heading</heading>
-        <content>
-          <p>Another important provision.</p>
-        </content>
-      </section>
-      <section id="section-30">
-        <num>30.</num>
-        <heading>More important</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-      <section id="section-31">
-        <num>31.</num>
-        <heading>More important</heading>
-        <content>
-          <p>Hi!</p>
-        </content>
-      </section>
-        """
-            ),
-            language=self.eng)
-
-        self.section_refs_finder.find_references_in_document(document)
-        root = etree.fromstring(expected.content)
-        expected.content = etree.tostring(root, encoding='utf-8').decode('utf-8')
-        self.assertEqual(expected.content, document.content)
-
     def test_section_basic(self):
         document = Document(
             document_xml=document_fixture(
@@ -663,6 +405,27 @@ class SectionRefsFinderTestCase(TestCase):
           <p>An unimportant provision.</p>
         </content>
       </section>
+      <section id="section-4">
+        <num>4.</num>
+        <heading>Unimportant</heading>
+        <content>
+          <p>Hi!</p>
+        </content>
+      </section>
+      <section id="section-5">
+        <num>5.</num>
+        <heading>Unimportant</heading>
+        <content>
+          <p>Hi!</p>
+        </content>
+      </section>
+      <section id="section-6">
+        <num>6.</num>
+        <heading>Unimportant</heading>
+        <content>
+          <p>Hi!</p>
+        </content>
+      </section>
       <section id="section-7">
         <num>7.</num>
         <heading>Active ref heading</heading>
@@ -677,6 +440,15 @@ class SectionRefsFinderTestCase(TestCase):
           <p>As given in section 26(2) and (3), blah.</p>
           <p>As given in sections 26 (2) and (3), blah.</p>
           <p>As given in sections 26(2) and (3), blah.</p>
+          <p>A person who contravenes sections 4(1), (2) and (3), 6(3) and 10(1) and (2) is guilty of an offence.</p>
+          <p>Subject to sections 1(4) and (5) and 4(6), no person is guilty of an offence.</p>
+        </content>
+      </section>
+      <section id="section-10">
+        <num>10.</num>
+        <heading>Unimportant</heading>
+        <content>
+          <p>Hi!</p>
         </content>
       </section>
       <section id="section-26">
@@ -728,6 +500,27 @@ class SectionRefsFinderTestCase(TestCase):
           <p>An unimportant provision.</p>
         </content>
       </section>
+      <section id="section-4">
+        <num>4.</num>
+        <heading>Unimportant</heading>
+        <content>
+          <p>Hi!</p>
+        </content>
+      </section>
+      <section id="section-5">
+        <num>5.</num>
+        <heading>Unimportant</heading>
+        <content>
+          <p>Hi!</p>
+        </content>
+      </section>
+      <section id="section-6">
+        <num>6.</num>
+        <heading>Unimportant</heading>
+        <content>
+          <p>Hi!</p>
+        </content>
+      </section>
       <section id="section-7">
         <num>7.</num>
         <heading>Active ref heading</heading>
@@ -742,6 +535,15 @@ class SectionRefsFinderTestCase(TestCase):
           <p>As given in <ref href="#section-26">section 26</ref>(2) and (3), blah.</p>
           <p>As given in <ref href="#section-26">sections 26</ref> (2) and (3), blah.</p>
           <p>As given in <ref href="#section-26">sections 26</ref>(2) and (3), blah.</p>
+          <p>A person who contravenes sections <ref href="#section-4">4</ref>(1), (2) and (3), <ref href="#section-6">6</ref>(3) and <ref href="#section-10">10</ref>(1) and (2) is guilty of an offence.</p>
+          <p>Subject to sections <ref href="#section-1">1</ref>(4) and (5) and <ref href="#section-4">4</ref>(6), no person is guilty of an offence.</p>
+        </content>
+      </section>
+      <section id="section-10">
+        <num>10.</num>
+        <heading>Unimportant</heading>
+        <content>
+          <p>Hi!</p>
         </content>
       </section>
       <section id="section-26">

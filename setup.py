4132150357cefade5f69811d5ec68b237c21f733
Greg Kempe <greg@kempe.net>
2020-01-10 12:49:53 +0200

    Cobalt 3.1.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     install_requires=[
         'django>=1.11.27,<2',
         'boto3>=1.7',
-        'cobalt>=3.0',
+        'cobalt>=3.1',
         'django-ckeditor>=5.8',
         'dj-database-url>=0.3.0',
         'django-activity-stream>=0.7.0',

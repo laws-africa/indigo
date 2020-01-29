3e9852f7dd9d53a7231a0e2c51fb548aab406e7c
goose-life <32566441+goose-life@users.noreply.github.com>
2020-01-29 14:39:11 +0200

    can_delete updated

--- indigo_api/models/works.py
+++ indigo_api/models/works.py
@@ -227,7 +227,8 @@ class Work(models.Model):
         return (not self.document_set.undeleted().exists() and
                 not self.child_works.exists() and
                 not self.repealed_works.exists() and
-                not self.commenced_works.exists() and
+                not self.commencements_made.exists() and
+                not [c for c in self.commencements.all() if c.commencing_work] and
                 not Amendment.objects.filter(Q(amending_work=self) | Q(amended_work=self)).exists())
 
     def create_expression_at(self, user, date, language=None):

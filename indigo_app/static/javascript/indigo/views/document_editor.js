c79dc50255c469db73cadb59a86e64863da90c67
Greg Kempe <greg@kempe.net>
2020-02-13 12:50:13 +0200

    FIX for xslt params

--- indigo_app/static/javascript/indigo/views/document_editor.js
+++ indigo_app/static/javascript/indigo/views/document_editor.js
@@ -408,9 +408,9 @@
         self.htmlTransform.setParameter(null, 'mediaUrl', self.parent.model.url() + '/');
         self.htmlTransform.setParameter(null, 'lang', self.parent.model.get('language'));
         self.htmlTransform.setParameter(null, 'documentType', self.parent.model.work.get('nature'));
-        self.htmlTransform.setParameter(null, 'subtype', self.parent.model.get('subtype'));
+        self.htmlTransform.setParameter(null, 'subtype', self.parent.model.get('subtype') || '');
         self.htmlTransform.setParameter(null, 'country', self.parent.model.work.get('country'));
-        self.htmlTransform.setParameter(null, 'locality', self.parent.model.work.get('language'));
+        self.htmlTransform.setParameter(null, 'locality', self.parent.model.work.get('locality') || '');
         var html = self.htmlTransform.transformToFragment(self.parent.fragment, document);
 
         self.makeLinksExternal(html);

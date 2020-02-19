9e36884ecd12fee640a651e0781b0c6900fed100
goose-life <32566441+goose-life@users.noreply.github.com>
2020-02-19 20:48:29 +0200

    make commenced provisions string beautiful

--- indigo_api/templatetags/indigo.py
+++ indigo_api/templatetags/indigo.py
@@ -23,9 +23,45 @@ def work_resolver_url(context, work):
 
 @register.simple_tag
 def commenced_provisions_description(document, commencement, uncommenced=False):
-    language = document.language
+    work = document.work
     if uncommenced:
         uncommenced_provisions = document.work.uncommenced_provisions()
+        # TODO: same as below for uncommenced provisions
         return ', '.join([p.id for p in uncommenced_provisions])
 
-    return ', '.join(commencement.provisions)
+    commenceable_provisions = work.commenceable_provisions()
+    commenced_provisions = commencement.provisions
+    runs = []
+    current_run = []
+
+    def end_current_run(run):
+        if len(run) < 4:
+            run = [f'{item["type"]} {item["num"]}' for item in run]
+        else:
+            run = [f'{run[0]["type"]}s {run[0]["num"]} to {run[-1]["num"]}']
+        runs.append(run)
+
+    for i, p in enumerate(commenceable_provisions):
+        if i == 0:
+            if p.id in commenced_provisions:
+                current_run.append({'type': p.type, 'num': p.num.split('.')[0] if p.num else ''})
+        else:
+            previous = commenceable_provisions[i - 1]
+            if p.id not in commenced_provisions and previous.id in commenced_provisions:
+                end_current_run(current_run)
+                current_run = []
+            elif p.id in commenced_provisions:
+                if not current_run:
+                    # new run
+                    current_run = [{'type': p.type, 'num': p.num.split('.')[0] if p.num else ''}]
+                elif p.type == previous.type:
+                    # current run is safe to add to (same type)
+                    current_run.append({'type': p.type, 'num': p.num.split('.')[0] if p.num else ''})
+                else:
+                    # current run is not safe to add to (different type)
+                    end_current_run(current_run)
+                    current_run = [{'type': p.type, 'num': p.num.split('.')[0] if p.num else ''}]
+    # we've finished checking all commenceable provisions
+    end_current_run(current_run)
+
+    return ', '.join(', '.join(r) for r in runs)

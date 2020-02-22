dc8b8d17d85a001d4f308a0bd93e18363b989e6c
goose-life <32566441+goose-life@users.noreply.github.com>
2020-02-23 01:41:29 +0200

    use same function for commenced and uncommenced provisions

--- indigo_api/templatetags/indigo.py
+++ indigo_api/templatetags/indigo.py
@@ -23,45 +23,56 @@ def work_resolver_url(context, work):
 
 @register.simple_tag
 def commenced_provisions_description(work, commencement, uncommenced=False):
-    if uncommenced:
-        uncommenced_provisions = work.uncommenced_provisions()
-        # TODO: same as below for uncommenced provisions
-        return ', '.join([p.id for p in uncommenced_provisions])
-
     commenceable_provisions = work.commenceable_provisions()
-    commenced_provisions = commencement.provisions
-    runs = []
-    current_run = []
 
-    def end_current_run(run):
-        if len(run) < 4:
-            run = [f'{item["type"]} {item["num"]}' for item in run]
-        else:
-            run = [f'{run[0]["type"]} {run[0]["num"]}–{run[-1]["num"]}']
-        runs.append(run)
+    def make_beautiful(provisions):
+        runs = []
+        current_run = []
 
-    for i, p in enumerate(commenceable_provisions):
-        if i == 0:
-            if p.id in commenced_provisions:
-                current_run.append({'type': p.type, 'num': p.num.strip('.')[0] if p.num else ''})
-        else:
+        def end_current(run):
+            if len(run) < 4:
+                run = [f'{item["type"]} {item["num"]}' for item in run]
+            else:
+                run = [f'{run[0]["type"]} {run[0]["num"]}–{run[-1]["num"]}']
+            runs.append(run)
+            return []
+
+        def add_to_current(element, run):
+            run.append({'type': element.type, 'num': element.num.strip('.')[0]})
+            return run
+
+        for i, p in enumerate(commenceable_provisions):
+            # first one only
+            if i == 0:
+                if p.id in provisions:
+                    current_run = add_to_current(p, current_run)
+                continue
+
+            # rest
             previous = commenceable_provisions[i - 1]
-            if p.id not in commenced_provisions and previous.id in commenced_provisions:
-                end_current_run(current_run)
-                current_run = []
-            elif p.id in commenced_provisions:
-                if not current_run:
-                    # new run
-                    current_run = [{'type': p.type, 'num': p.num.strip('.')[0] if p.num else ''}]
-                elif p.type == previous.type:
-                    # current run is safe to add to (same type)
-                    current_run.append({'type': p.type, 'num': p.num.strip('.')[0] if p.num else ''})
-                else:
-                    # current run is not safe to add to (different type)
-                    end_current_run(current_run)
-                    current_run = [{'type': p.type, 'num': p.num.strip('.')[0] if p.num else ''}]
-    # we've finished checking all commenceable provisions; add the last group (but not if it's empty)
-    if current_run:
-        end_current_run(current_run)
 
-    return ', '.join(', '.join(r) for r in runs)
+            if p.id not in provisions and previous.id in provisions:
+                current_run = end_current(current_run)
+
+            elif p.id in provisions:
+                if not current_run or p.type == previous.type:
+                    # current run is safe to add to (same type, or empty)
+                    current_run = add_to_current(p, current_run)
+                else:
+                    # current run is not safe to add to, start a new one
+                    current_run = end_current(current_run)
+                    current_run = add_to_current(p, current_run)
+
+        # we've finished checking all commenceable provisions; add the last group
+        # (but not if it's empty)
+        if current_run:
+            end_current(current_run)
+
+        return runs
+
+    if uncommenced:
+        provisions = [p.id for p in work.uncommenced_provisions()]
+    else:
+        provisions = commencement.provisions
+
+    return ', '.join(', '.join(p) for p in make_beautiful(provisions))

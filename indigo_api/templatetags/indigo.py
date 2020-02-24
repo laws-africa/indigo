9b1e332a277a56a2c24552682c8b4952426d3c43
goose-life <32566441+goose-life@users.noreply.github.com>
2020-02-24 14:50:27 +0200

    tighten up make_beautiful

--- indigo_api/templatetags/indigo.py
+++ indigo_api/templatetags/indigo.py
@@ -21,47 +21,45 @@ def work_resolver_url(context, work):
     return context.get('resolver_url', settings.RESOLVER_URL) + frbr_uri
 
 
-def make_beautiful(work, provisions):
-    commenceable_provisions = work.commenceable_provisions()
+def make_beautiful(provisions, commenceable_provisions):
     runs = []
     current_run = []
+    previous = None
 
-    def add_to_current(element, run):
-        run.append({'type': element.type, 'num': element.num.strip('.')[0]})
-        return run
+    def add_to_current(element):
+        current_run.append({'type': element.type, 'num': element.num.strip('.')})
 
-    def end_current(run):
-        if len(run) < 4:
-            run = [f'{item["type"]} {item["num"]}' for item in run]
+    def end_current():
+        if len(current_run) < 4:
+            run = [f'{item["type"]} {item["num"]}' for item in current_run]
         else:
-            run = [f'{run[0]["type"]} {run[0]["num"]}–{run[-1]["num"]}']
+            run = [f'{current_run[0]["type"]} {current_run[0]["num"]}–{current_run[-1]["num"]}']
         runs.append(run)
         return []
 
-    for i, p in enumerate(commenceable_provisions):
-        if i > 0:
-            previous = commenceable_provisions[i - 1]
-
+    for p in commenceable_provisions:
         if p.id in provisions:
-            if i == 0:
-                current_run = add_to_current(p, current_run)
+            if not previous:
+                add_to_current(p)
+                previous = p
                 continue
 
             if previous.id in provisions and previous.type != p.type:
                 # current run is not safe to add to (different type)
-                current_run = end_current(current_run)
+                current_run = end_current()
+            add_to_current(p)
 
-            current_run = add_to_current(p, current_run)
+        elif previous and previous.id in provisions:
+            current_run = end_current()
 
-        elif i > 0 and previous.id in provisions:
-            current_run = end_current(current_run)
+        previous = p
 
     # we've finished checking all commenceable provisions; add the last group
     # (but not if it's empty)
     if current_run:
-        end_current(current_run)
+        end_current()
 
-    return runs
+    return ', '.join(', '.join(p) for p in runs)
 
 
 @register.simple_tag
@@ -71,4 +69,6 @@ def commenced_provisions_description(work, commencement, uncommenced=False):
     else:
         provisions = commencement.provisions
 
-    return ', '.join(', '.join(p) for p in make_beautiful(work, provisions))
+    commenceable_provisions = work.commenceable_provisions()
+
+    return make_beautiful(provisions, commenceable_provisions)

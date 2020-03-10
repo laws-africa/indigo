19767f80136245c0c93b74d7a248b7c7a82d76a6
Greg Kempe <greg@kempe.net>
2020-03-10 13:24:37 +0300

    Log and email admins when tasks fail
    Otherwise, they fail silently.

--- indigo_metrics/tasks.py
+++ indigo_metrics/tasks.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, date
+import logging
 
 from background_task import background
 from background_task.models import Task
@@ -7,14 +8,22 @@ from django.utils import timezone
 from indigo_metrics.models import DailyWorkMetrics, WorkMetrics
 
 
+# get specific task logger
+log = logging.getLogger('indigo.tasks')
+
+
 @background(queue="indigo", remove_existing_tasks=True)
 def update_yesterdays_metrics():
     """ Task to update daily metrics for yesterday. This will replace any existing task.
     """
-    WorkMetrics.update_all_work_metrics()
+    try:
+        WorkMetrics.update_all_work_metrics()
 
-    yesterday = date.today() - timedelta(days=1)
-    DailyWorkMetrics.update_daily_work_metrics(yesterday)
+        yesterday = date.today() - timedelta(days=1)
+        DailyWorkMetrics.update_daily_work_metrics(yesterday)
+    except Exception as e:
+        log.error(f"Error updating work metrics: {e}", exc_info=e)
+        raise e
 
 
 def setup_update_metrics_task(hour=1):

64bd7aa91eb5ae7fd7365a8c797cd6a733e268d5
Robley <robleyadrian@gmail.com>
2020-03-16 15:08:25 +0300

    move completeness chart JS from place_works to place_detail

--- indigo_app/static/javascript/indigo/views/place_works.js
+++ indigo_app/static/javascript/indigo/views/place_works.js
@@ -35,63 +35,63 @@
            .toggleClass('fa-caret-down', opened);
     },
 
-    drawCompletenessChart: function() {
-        var canvas = document.getElementById('completeness-chart'),
-            ctx = canvas.getContext('2d'),
-            data = _.map(canvas.getAttribute('data-values').split(','), function(i) { return parseInt(i); });
+    // drawCompletenessChart: function() {
+    //     var canvas = document.getElementById('completeness-chart'),
+    //         ctx = canvas.getContext('2d'),
+    //         data = _.map(canvas.getAttribute('data-values').split(','), function(i) { return parseInt(i); });
 
-        new Chart(ctx, {
-          type: 'line',
-          data: {
-            labels: data,
-            datasets: [{
-              label: 'Completeness',
-              data: data,
-              backgroundColor: 'rgba(67, 159, 120, 0.2)',
-              borderColor: 'rgba(67, 159, 120, 1)',
-              borderWidth: 2,
-              fill: 'start',
-            }]
-          },
-          options: {
-            maintainAspectRatio: false,
-            tooltips: {
-              callbacks: {
-                title: function(items, data) { return items[0].value + '%'; },
-                label: function(item, data) { return; },
-                beforeLabel: function(item, data) { return; },
-              },
-            },
-            layout: {
-              padding: {
-                top: 2,
-                left: 2,
-                right: 2,
-                bottom: 1,
-              }
-            },
-            legend: {display: false},
-            elements: {
-              line: {
-                tension: 0.4,
-              },
-              point: {
-                radius: 0,
-              },
-            },
-            scales: {
-              xAxes: [{display: false}],
-              yAxes: [{
-                display: false,
-                ticks: {
-                  min: 0,
-                  max: 100,
-                  beginAtZero: true,
-                }
-              }]
-            }
-          }
-        });
-    },
+    //     new Chart(ctx, {
+    //       type: 'line',
+    //       data: {
+    //         labels: data,
+    //         datasets: [{
+    //           label: 'Completeness',
+    //           data: data,
+    //           backgroundColor: 'rgba(67, 159, 120, 0.2)',
+    //           borderColor: 'rgba(67, 159, 120, 1)',
+    //           borderWidth: 2,
+    //           fill: 'start',
+    //         }]
+    //       },
+    //       options: {
+    //         maintainAspectRatio: false,
+    //         tooltips: {
+    //           callbacks: {
+    //             title: function(items, data) { return items[0].value + '%'; },
+    //             label: function(item, data) { return; },
+    //             beforeLabel: function(item, data) { return; },
+    //           },
+    //         },
+    //         layout: {
+    //           padding: {
+    //             top: 2,
+    //             left: 2,
+    //             right: 2,
+    //             bottom: 1,
+    //           }
+    //         },
+    //         legend: {display: false},
+    //         elements: {
+    //           line: {
+    //             tension: 0.4,
+    //           },
+    //           point: {
+    //             radius: 0,
+    //           },
+    //         },
+    //         scales: {
+    //           xAxes: [{display: false}],
+    //           yAxes: [{
+    //             display: false,
+    //             ticks: {
+    //               min: 0,
+    //               max: 100,
+    //               beginAtZero: true,
+    //             }
+    //           }]
+    //         }
+    //       }
+    //     });
+    // },
   });
 })(window);

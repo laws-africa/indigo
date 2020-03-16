5d47b8c9a1900b41ea765dab732adcc2e1fdb92b
Robley <robleyadrian@gmail.com>
2020-03-16 17:34:25 +0300

    remove redundant code

--- indigo_app/static/javascript/indigo/views/place_works.js
+++ indigo_app/static/javascript/indigo/views/place_works.js
@@ -13,14 +13,6 @@
       'hidden.bs.collapse .work-extra-detail': 'workDetailToggled',
     },
 
-    initialize: function() {
-      this.drawCharts();
-    },
-
-    drawCharts: function() {
-      this.drawCompletenessChart();
-    },
-
     linkClicked: function(e) {
       // don't bubble to avoid collapsing the container unnecessarily
       e.stopPropagation();
@@ -34,64 +26,5 @@
       $icon.toggleClass('fa-caret-right', !opened)
            .toggleClass('fa-caret-down', opened);
     },
-
-    // drawCompletenessChart: function() {
-    //     var canvas = document.getElementById('completeness-chart'),
-    //         ctx = canvas.getContext('2d'),
-    //         data = _.map(canvas.getAttribute('data-values').split(','), function(i) { return parseInt(i); });
-
-    //     new Chart(ctx, {
-    //       type: 'line',
-    //       data: {
-    //         labels: data,
-    //         datasets: [{
-    //           label: 'Completeness',
-    //           data: data,
-    //           backgroundColor: 'rgba(67, 159, 120, 0.2)',
-    //           borderColor: 'rgba(67, 159, 120, 1)',
-    //           borderWidth: 2,
-    //           fill: 'start',
-    //         }]
-    //       },
-    //       options: {
-    //         maintainAspectRatio: false,
-    //         tooltips: {
-    //           callbacks: {
-    //             title: function(items, data) { return items[0].value + '%'; },
-    //             label: function(item, data) { return; },
-    //             beforeLabel: function(item, data) { return; },
-    //           },
-    //         },
-    //         layout: {
-    //           padding: {
-    //             top: 2,
-    //             left: 2,
-    //             right: 2,
-    //             bottom: 1,
-    //           }
-    //         },
-    //         legend: {display: false},
-    //         elements: {
-    //           line: {
-    //             tension: 0.4,
-    //           },
-    //           point: {
-    //             radius: 0,
-    //           },
-    //         },
-    //         scales: {
-    //           xAxes: [{display: false}],
-    //           yAxes: [{
-    //             display: false,
-    //             ticks: {
-    //               min: 0,
-    //               max: 100,
-    //               beginAtZero: true,
-    //             }
-    //           }]
-    //         }
-    //       }
-    //     });
-    // },
   });
 })(window);

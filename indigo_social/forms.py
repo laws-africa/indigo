732cb969e05b4059207bbfca9067ece0b2c91637
Robley Gori <robleyadrian@gmail.com>
2020-04-16 16:47:43 +0300

    add process_profile_photo() to handle new images

--- indigo_social/forms.py
+++ indigo_social/forms.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+import sys
+from io import BytesIO
+
 from django import forms
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.validators import _lazy_re_compile, RegexValidator
+from PIL import Image
 
 from indigo_api.models import Country, User
 from indigo_social.models import UserProfile
@@ -50,7 +55,8 @@ class UserProfileForm(forms.ModelForm):
 
     def save(self, commit=True):
         if self.cleaned_data.get('new_profile_photo'):
-            self.instance.profile_photo = self.cleaned_data['new_profile_photo']
+            self.instance.profile_photo = process_profile_photo(self.cleaned_data['new_profile_photo'])
+            self.instance.generate_nonce()
 
         if '_delete_photo' in self.data:
             self.instance.profile_photo.delete(save=False)
@@ -67,6 +73,39 @@ class UserProfileForm(forms.ModelForm):
 
 def badge_choices():
     return sorted([(b.slug, '%s – %s' % (b.name, b.description)) for b in badges.registry.values() if b.can_award_manually])
+
+
+def process_profile_photo(profile_photo):
+    """ Process uploaded profile photo to meet specifications as follows:
+    - crop into a square if image is not
+    - resize to 200*200
+    - convert to jpeg
+    """
+    uploaded_photo = Image.open(profile_photo)
+    file_name = profile_photo.name.split('.')[0]
+    output = BytesIO()
+    width, height = uploaded_photo.size
+
+    if width != height:
+        new_width = min(width, height)
+
+        left = (width - new_width)/2
+        top = (height - new_width)/2
+        right = (width + new_width)/2
+        bottom = (height + new_width)/2
+        uploaded_photo = uploaded_photo.crop((left, top, right, bottom))
+        uploaded_photo = uploaded_photo.resize((200, 200), Image.ANTIALIAS)
+    else:
+        uploaded_photo = uploaded_photo.resize((200, 200), Image.ANTIALIAS)
+
+    if not uploaded_photo.mode == 'RGB':
+        uploaded_photo = uploaded_photo.convert('RGB')
+
+    uploaded_photo.save(output, format='jpeg', quality=100)
+    output.seek(0)
+    resized_profile_photo = InMemoryUploadedFile(output, 'ImageField', '{}.jpeg'.format(file_name),
+                                                 'image/jpeg', sys.getsizeof(output), None)
+    return resized_profile_photo
 
 
 class AwardBadgeForm(forms.Form):

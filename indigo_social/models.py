15bdb4902e0543473c8fc8f8d3fe1939e9ef58f3
Robley Gori <robleyadrian@gmail.com>
2020-04-16 13:54:34 +0300

    generate nonce when importing google photos, move resize_photo()

--- indigo_social/models.py
+++ indigo_social/models.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
-from io import BytesIO
-import sys
+import random
+import string
 
 from django.contrib.auth.models import User
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.core.files import File
-from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
-from PIL import Image
+
 
 from urllib.request import urlretrieve, urlcleanup
 
@@ -20,8 +20,9 @@ from allauth.socialaccount.signals import pre_social_login, social_account_added
 
 
 def user_profile_photo_path(instance, filename):
-    file_name, file_extension = os.path.splitext(filename)
-    return 'avatars/{}'.format(instance.id)
+    if instance.id is not None:
+        return 'avatars/{}'.format(instance.id)
+    raise ValueError
 
 
 def retrieve_social_profile_photo(user_profile, url):
@@ -30,32 +31,9 @@ def retrieve_social_profile_photo(user_profile, url):
     try:
         filename, _ = urlretrieve(url)
         user_profile.profile_photo.save(filename, File(open(filename, 'rb')))
+        user_profile.generate_nonce()
     finally:
         urlcleanup()
-
-
-def resize_photo(profile_photo, user):
-    """ Resize and crop an uploaded profile photo to meet specifications
-    """
-    uploaded_profile_photo = Image.open(profile_photo)
-    file_extension = profile_photo.name.split('.')[1]
-    file_name = user.pk
-    output = BytesIO()
-
-    # TODO: crop then resize
-    # profile_photo = uploaded_profile_photo.crop((0, 0, 200, 200))
-    profile_photo = uploaded_profile_photo.resize((200, 200), Image.ANTIALIAS)
-    profile_photo.save(output, format=file_extension, quality=100)
-    output.seek(0)
-
-    profile_photo = InMemoryUploadedFile(output, 
-                                         'ImageField', 
-                                         "{}.{}".format(file_name, file_extension),
-                                         'image/{}'.format(file_extension), 
-                                          sys.getsizeof(output),
-                                          None)
-
-    return profile_photo
 
 
 class UserProfile(models.Model):
@@ -71,17 +49,15 @@ class UserProfile(models.Model):
     twitter_username = models.CharField(max_length=256, blank=True, null=True, default='')
     linkedin_profile = models.URLField(blank=True, null=True, default='')
 
-    def save(self):
-        # TODO: Generate and save the nonce here
-        self.profile_photo = resize_photo(self.profile_photo, self.user)
-
-        super(UserProfile, self).save()
+    def generate_nonce(self):
+        self.profile_photo_nonce = ''.join(random.sample(string.ascii_lowercase, 8))
+        self.save()
 
     @property
     def profile_photo_url(self):
         if not self.profile_photo:
-            return "/static/images/avatars/default_avatar.svg"
-        return reverse('indigo_social:user_profile_photo', kwargs={'username': self.user.username, 'nonce': '123'})
+            return static('images/avatars/default_avatar.svg')
+        return reverse('indigo_social:user_profile_photo', kwargs={'username': self.user.username, 'nonce': self.profile_photo_nonce})
     
 
 @receiver(post_save, sender=User)
@@ -98,10 +74,9 @@ def retrieve_profile_photo_on_signup(sender, **kwargs):
     user = kwargs['user']
 
     try:
-        user_profile = UserProfile.objects.get(user=user)
         user_social_account = SocialAccount.objects.get(user=user, provider='google')
         url = user_social_account.extra_data.get('picture')
-        retrieve_social_profile_photo(user_profile, url)
+        retrieve_social_profile_photo(user.userprofile, url)
 
     except (SocialAccount.DoesNotExist, UserProfile.DoesNotExist):
         pass

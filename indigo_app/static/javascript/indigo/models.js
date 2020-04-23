3171e3f7aa3341a9fff6802e0498a4c5c54ba1a7
goose-life <32566441+goose-life@users.noreply.github.com>
2020-04-23 14:58:47 +0200

    update Indigo.Document.newFromFrbrUri to assume /akn prefix

--- indigo_app/static/javascript/indigo/models.js
+++ indigo_app/static/javascript/indigo/models.js
@@ -422,19 +422,19 @@
 
   /** Create a new document by parsing an frbr URI */
   Indigo.Document.newFromFrbrUri = function(frbr_uri) {
-    // /za-cpt/act/by-law/2011/foo
+    // /akn/za-cpt/act/by-law/2011/foo
     var parts = frbr_uri.split('/'),
-        tmp = parts[1].split('-'),
-        country = tmp[0],
-        locality = tmp.length > 1 ? tmp[1] : null,
-        bump = parts.length > 5 ? 1 : 0;
+        place = parts[2].split('-'),
+        country = place[0],
+        locality = place.length > 1 ? place[1] : null,
+        bump = parts.length > 6 ? 1 : 0;
 
     return new Indigo.Document({
       country: country,
       locality: locality,
-      subtype: parts.length > 5 ? parts[3] : null,
-      year: parts[3 + bump],
-      number: parts[4 + bump],
+      subtype: parts.length > 6 ? parts[4] : null,
+      year: parts[4 + bump],
+      number: parts[5 + bump],
     });
   };
 

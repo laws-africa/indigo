e60e5967b343902f31999de1355fb8a38e9f482f
Robley Gori <robleyadrian@gmail.com>
2020-05-08 10:07:22 +0300

    Update indigo_app/static/javascript/indigo/views/task_detail.js
    Co-authored-by: Greg Kempe <greg@kempe.net>

--- indigo_app/static/javascript/indigo/views/task_detail.js
+++ indigo_app/static/javascript/indigo/views/task_detail.js
@@ -34,7 +34,7 @@
       if (text.length > 0) {
         this.$('#btn_request_changes').val('Comment and request changes');
         this.$('#btn_approve').val('Comment and approve');
-        this.$('#btn_reopen').val('Comment and Reopen');
+        this.$('#btn_reopen').val('Comment and reopen');
         this.$('#btn_submit_for_review').val('Comment and Submit for Review');
 
       } else {

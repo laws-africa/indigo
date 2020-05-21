9ce1b39efb9864dcd4b245ad7fc4b3960ae860a1
goose-life <32566441+goose-life@users.noreply.github.com>
2020-05-21 22:29:48 +0200

    don't use safe_update when replacing . separator with __

--- indigo_api/data_migrations/__init__.py
+++ indigo_api/data_migrations/__init__.py
@@ -293,7 +293,8 @@ class AKNeId(AKNMigration):
             old_id = node.get("id")
             if "." in old_id:
                 new_id = old_id.replace(".", "__")
-                self.safe_update(node, mappings, old_id, new_id)
+                node.set("id", new_id)
+                mappings.update({old_id: new_id})
 
         # replace all `id`s with `eId`s
         for node in doc.root.xpath("//a:*[@id]", namespaces=nsmap):

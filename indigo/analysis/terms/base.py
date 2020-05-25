6d3a669e0c04da11d9fe91ed6e6c3104c27cd33e
goose-life <32566441+goose-life@users.noreply.github.com>
2020-05-25 16:41:07 +0200

    update renumber_terms, tests

--- indigo/analysis/terms/base.py
+++ indigo/analysis/terms/base.py
@@ -1,5 +1,6 @@
 import re
 import logging
+from collections import Counter
 from itertools import chain
 from lxml import etree
 
@@ -239,5 +240,16 @@ class BaseTermsFinder(LocaleBasedMatcher):
     def renumber_terms(self, doc):
         """ Recalculate ids for <term> elements
         """
-        for i, term in enumerate(doc.xpath('//a:term', namespaces=self.nsmap)):
-            term.set('id', "trm%s" % i)
+
+        def get_parent_id(node):
+            parent = node.getparent()
+            parent_id = parent.get("eId")
+            return parent_id if parent_id else get_parent_id(parent)
+
+        # generate correct term ids, based on the parent id
+        counter = Counter()
+        for node in doc.xpath('//a:term', namespaces=self.nsmap):
+            prefix = get_parent_id(node)
+            # the num for this term depends on the number of preceding terms with the same prefix
+            counter[prefix] += 1
+            node.set('eId', f'{prefix}__term_{counter[prefix]}')

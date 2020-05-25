5ad9ee6b4de2d5ce56f4d48dc49596e972d4a249
Robley G <robleyadrian@gmail.com>
2020-05-25 12:11:16 +0300

    refactor term tests to match new eIds and sec_1 eId format

--- indigo_za/tests/test_terms_eng.py
+++ indigo_za/tests/test_terms_eng.py
@@ -16,20 +16,20 @@ class TermsFinderENGTestCase(APITestCase):
 
     def test_find_simple(self):
         doc = Document(work=self.work, content=document_fixture(xml="""
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>In this By-law, unless the context indicates otherwise-</p>
       <p>"Act" means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
-      <blockList id="section-1.paragraph-0.list3">
+      <blockList eId="sec_1.paragraph-0.list3">
         <listIntroduction>"authorised officer" includes-</listIntroduction>
-        <item id="section-1.paragraph-0.list3.a">
+        <item eId="sec_1.paragraph-0.list3.a">
           <num>(a)</num>
           <p>a person in the service of the City whose duty is to inspect licences, examine vehicles, examine driving licences, or who is a traffic officer or a road traffic law enforcement officer, and also any other person declared by the Minister of Transport by regulation made in terms of the National Road Traffic Act to be an authorised officer; and</p>
         </item>
-        <item id="section-1.paragraph-0.list3.b">
+        <item eId="sec_1.paragraph-0.list3.b">
           <num>(b)</num>
           <p>a person appointed as an inspector by the City as contemplated in section 86 of the National Land Transport Act, 2009 (Act No. 5 of 2009);</p>
         </item>
@@ -42,22 +42,22 @@ class TermsFinderENGTestCase(APITestCase):
         self.finder.find_terms_in_document(doc)
         self.assertMultiLineEqual('''<body xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
       
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>In this By-law, unless the context indicates otherwise-</p>
       <p refersTo="#term-Act">"<def refersTo="#term-Act">Act</def>" means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
-      <blockList id="section-1.paragraph-0.list3" refersTo="#term-authorised_officer">
+      <blockList eId="sec_1.paragraph-0.list3" refersTo="#term-authorised_officer">
         <listIntroduction>"<def refersTo="#term-authorised_officer">authorised officer</def>" includes-</listIntroduction>
-        <item id="section-1.paragraph-0.list3.a">
+        <item eId="sec_1.paragraph-0.list3.a">
           <num>(a)</num>
-          <p>a person in the service of the City whose duty is to inspect licences, examine vehicles, examine driving licences, or who is a traffic officer or a road traffic law enforcement officer, and also any other person declared by the Minister of Transport by regulation made in terms of the National Road Traffic <term refersTo="#term-Act" id="trm0">Act</term> to be an authorised officer; and</p>
+          <p>a person in the service of the City whose duty is to inspect licences, examine vehicles, examine driving licences, or who is a traffic officer or a road traffic law enforcement officer, and also any other person declared by the Minister of Transport by regulation made in terms of the National Road Traffic <term refersTo="#term-Act" eId="trm0">Act</term> to be an authorised officer; and</p>
         </item>
-        <item id="section-1.paragraph-0.list3.b">
+        <item eId="sec_1.paragraph-0.list3.b">
           <num>(b)</num>
-          <p>a person appointed as an inspector by the City as contemplated in section 86 of the National Land Transport <term refersTo="#term-Act" id="trm1">Act</term>, 2009 (<term refersTo="#term-Act" id="trm2">Act</term> No. 5 of 2009);</p>
+          <p>a person appointed as an inspector by the City as contemplated in section 86 of the National Land Transport <term refersTo="#term-Act" eId="trm1">Act</term>, 2009 (<term refersTo="#term-Act" eId="trm2">Act</term> No. 5 of 2009);</p>
         </item>
       </blockList>
     </content>
@@ -69,20 +69,20 @@ class TermsFinderENGTestCase(APITestCase):
 
     def test_find_no_heading(self):
         doc = Document(work=self.work, content=document_fixture(xml="""
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading/>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>In this By-law, unless the context indicates otherwise-</p>
       <p>"Act" means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
-      <blockList id="section-1.paragraph-0.list3">
+      <blockList eId="sec_1.paragraph-0.list3">
         <listIntroduction>"authorised officer" includes-</listIntroduction>
-        <item id="section-1.paragraph-0.list3.a">
+        <item eId="sec_1.paragraph-0.list3.a">
           <num>(a)</num>
           <p>a person in the service of the City whose duty is to inspect licences, examine vehicles, examine driving licences, or who is a traffic officer or a road traffic law enforcement officer, and also any other person declared by the Minister of Transport by regulation made in terms of the National Road Traffic Act to be an authorised officer; and</p>
         </item>
-        <item id="section-1.paragraph-0.list3.b">
+        <item eId="sec_1.paragraph-0.list3.b">
           <num>(b)</num>
           <p>a person appointed as an inspector by the City as contemplated in section 86 of the National Land Transport Act, 2009 (Act No. 5 of 2009);</p>
         </item>
@@ -95,20 +95,20 @@ class TermsFinderENGTestCase(APITestCase):
         self.finder.find_terms_in_document(doc)
         self.assertMultiLineEqual('''<body xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
       
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading/>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>In this By-law, unless the context indicates otherwise-</p>
       <p>"Act" means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
-      <blockList id="section-1.paragraph-0.list3">
+      <blockList eId="sec_1.paragraph-0.list3">
         <listIntroduction>"authorised officer" includes-</listIntroduction>
-        <item id="section-1.paragraph-0.list3.a">
+        <item eId="sec_1.paragraph-0.list3.a">
           <num>(a)</num>
           <p>a person in the service of the City whose duty is to inspect licences, examine vehicles, examine driving licences, or who is a traffic officer or a road traffic law enforcement officer, and also any other person declared by the Minister of Transport by regulation made in terms of the National Road Traffic Act to be an authorised officer; and</p>
         </item>
-        <item id="section-1.paragraph-0.list3.b">
+        <item eId="sec_1.paragraph-0.list3.b">
           <num>(b)</num>
           <p>a person appointed as an inspector by the City as contemplated in section 86 of the National Land Transport Act, 2009 (Act No. 5 of 2009);</p>
         </item>
@@ -122,10 +122,10 @@ class TermsFinderENGTestCase(APITestCase):
 
     def test_find_with_empty_string(self):
         doc = Document(work=self.work, content=document_fixture(xml="""
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading/>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>In this By-law, unless the context indicates otherwise-</p>
       <p>"" means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
@@ -137,10 +137,10 @@ class TermsFinderENGTestCase(APITestCase):
         self.finder.find_terms_in_document(doc)
         self.assertMultiLineEqual('''<body xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
       
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading/>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>In this By-law, unless the context indicates otherwise-</p>
       <p>"" means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
@@ -153,10 +153,10 @@ class TermsFinderENGTestCase(APITestCase):
 
     def test_unicode(self):
         doc = Document(work=self.work, content=document_fixture(xml="""
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>"Actë" means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
     </content>
@@ -167,10 +167,10 @@ class TermsFinderENGTestCase(APITestCase):
         self.finder.find_terms_in_document(doc)
         self.assertMultiLineEqual('''<body xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
       
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p refersTo="#term-Actë">"<def refersTo="#term-Actë">Actë</def>" means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
     </content>
@@ -182,10 +182,10 @@ class TermsFinderENGTestCase(APITestCase):
 
     def test_fancy_quotes(self):
         doc = Document(work=self.work, content=document_fixture(xml="""
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>“Act“ means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
     </content>
@@ -196,10 +196,10 @@ class TermsFinderENGTestCase(APITestCase):
         self.finder.find_terms_in_document(doc)
         self.assertMultiLineEqual('''<body xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
       
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p refersTo="#term-Act">“<def refersTo="#term-Act">Act</def>“ means the National Road Traffic Act, 1996 (<ref href="/za/act/1996/93">Act No. 93 of 1996</ref>);</p>
     </content>
@@ -211,16 +211,16 @@ class TermsFinderENGTestCase(APITestCase):
 
     def test_whitespace_between_adjacent_terms(self):
         doc = Document(work=self.work, content=document_fixture(xml="""
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>"fire" means a combustion;</p>
       <p>"truck" means a vehicle;</p>
     </content>
   </paragraph>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p>Do not park near a fire truck.</p>
     </content>
@@ -231,18 +231,18 @@ class TermsFinderENGTestCase(APITestCase):
         self.finder.find_terms_in_document(doc)
         self.assertMultiLineEqual('''<body xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
       
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
       <p refersTo="#term-fire">"<def refersTo="#term-fire">fire</def>" means a combustion;</p>
       <p refersTo="#term-truck">"<def refersTo="#term-truck">truck</def>" means a vehicle;</p>
     </content>
   </paragraph>
-  <paragraph id="section-1.paragraph-0">
+  <paragraph eId="sec_1.paragraph-0">
     <content>
-      <p>Do not park near a <term refersTo="#term-fire" id="trm0">fire</term> <term refersTo="#term-truck" id="trm1">truck</term>.</p>
+      <p>Do not park near a <term refersTo="#term-fire" eId="trm0">fire</term> <term refersTo="#term-truck" eId="trm1">truck</term>.</p>
     </content>
   </paragraph>
 </section>
@@ -252,10 +252,10 @@ class TermsFinderENGTestCase(APITestCase):
 
     def test_whitespace_between_adjacent_terms2(self):
         doc = Document(work=self.work, content=document_fixture(xml="""
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <subsection id="section-1.1">
+  <subsection eId="sec_1.1">
     <num>(1)</num>
     <content>
       <p>"keep" means something</p>
@@ -271,17 +271,17 @@ class TermsFinderENGTestCase(APITestCase):
         self.finder.find_terms_in_document(doc)
         self.assertMultiLineEqual('''<body xmlns="http://docs.oasis-open.org/legaldocml/ns/akn/3.0">
       
-<section id="section-1">
+<section eId="sec_1">
   <num>1.</num>
   <heading>Definitions</heading>
-  <subsection id="section-1.1">
+  <subsection eId="sec_1.1">
     <num>(1)</num>
     <content>
       <p refersTo="#term-keep">"<def refersTo="#term-keep">keep</def>" means something</p>
       <p refersTo="#term-game">"<def refersTo="#term-game">game</def>" means something</p>
-      <p><term refersTo="#term-keep" id="trm0">keep</term> <term refersTo="#term-game" id="trm1">game</term> or any other wild animal.</p>
-      <p>may <term refersTo="#term-keep" id="trm2">keep</term> <term refersTo="#term-game" id="trm3">game</term> or any other wild animal.</p>
-      <p>may not <term refersTo="#term-keep" id="trm4">keep</term> <term refersTo="#term-game" id="trm5">game</term></p>
+      <p><term refersTo="#term-keep" eId="trm0">keep</term> <term refersTo="#term-game" eId="trm1">game</term> or any other wild animal.</p>
+      <p>may <term refersTo="#term-keep" eId="trm2">keep</term> <term refersTo="#term-game" eId="trm3">game</term> or any other wild animal.</p>
+      <p>may not <term refersTo="#term-keep" eId="trm4">keep</term> <term refersTo="#term-game" eId="trm5">game</term></p>
     </content>
   </subsection>
 </section>

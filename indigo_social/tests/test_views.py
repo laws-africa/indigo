f8466797b01d468703758b0f48c1e9fba56b4438
Greg Kempe <greg@kempe.net>
2021-05-24 15:48:26 +0200

    Log in for social page tests

--- indigo_social/tests/test_views.py
+++ indigo_social/tests/test_views.py
@@ -8,6 +8,9 @@ from pinax.badges.registry import badges
 class SocialViewsTest(testcases.TestCase):
     fixtures = ['languages_data', 'countries', 'user', 'editor']
 
+    def setUp(self):
+        self.assertTrue(self.client.login(username='email@example.com', password='password'))
+
     def test_contributor_page(self):
         response = self.client.get('/contributors/')
         self.assertEqual(response.status_code, 200)

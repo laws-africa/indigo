59a5a71f0fd61dc79c0ec0e4492e59f8c674a02f
Greg Kempe <greg@kempe.net>
2021-11-03 11:52:23 +0200

    Refactor to make testing easier

--- indigo_api/data_migrations/__init__.py
+++ indigo_api/data_migrations/__init__.py
@@ -8,62 +8,72 @@ class RealCrossHeadings:
     """ Migrate old-style hcontainer cross-heading to crossHeading
     """
     def migrate_document(self, document):
+        self.ns = document.doc.namespace
         root = etree.fromstring(document.content)
-        if self.migrate_xml(root, document.doc.namespace):
+        if self.migrate_xml(root):
             document.content = etree.tostring(root, encoding='unicode')
             return True
 
-    def migrate_xml(self, xml, ns):
+    def migrate_xml(self, xml):
         changed = False
         parents = set()
         self.eid_mappings = {}
 
-        for hcontainer in xml.xpath('//a:hcontainer[@name="crossheading"]', namespaces={'a': ns}):
+        for hcontainer in xml.xpath('//a:hcontainer[@name="crossheading"]', namespaces={'a': self.ns}):
             changed = True
             parents.add(hcontainer.getparent())
-
-            # change to crossHeading
-            hcontainer.tag = '{%s}crossHeading' % ns
-            del hcontainer.attrib['name']
-
-            # unwrap heading text
-            heading = hcontainer.find('a:heading', {'a': ns})
-            unwrap_element(heading)
+            self.migrate_element(hcontainer)
 
         # re-write eIds, for both the changed elements and any conflicting siblings
         for parent in parents:
             # rewrite new crossHeadings
-            for i, crossHeading in enumerate(parent.xpath('./a:crossHeading', namespaces={'a': ns})):
+            for i, crossHeading in enumerate(parent.xpath('./a:crossHeading', namespaces={'a': self.ns})):
                 old_id = crossHeading.get('eId')
                 new_id = re.sub(r'hcontainer_\d+$', f'crossHeading_{i + 1}', old_id)
                 self.eid_mappings.update(rewrite_ids(crossHeading, old_id, new_id))
 
             # now re-write all hcontainers
-            for i, hcontainer in enumerate(parent.xpath('./a:hcontainer', namespaces={'a': ns})):
+            for i, hcontainer in enumerate(parent.xpath('./a:hcontainer', namespaces={'a': self.ns})):
                 old_id = hcontainer.get('eId')
                 new_id = re.sub(r'hcontainer_\d+$', f'hcontainer_{i + 1}', old_id)
                 self.eid_mappings.update(rewrite_ids(hcontainer, old_id, new_id))
 
         return changed
 
+    def migrate_element(self, hcontainer):
+        # change to crossHeading
+        hcontainer.tag = '{%s}crossHeading' % self.ns
+        del hcontainer.attrib['name']
+
+        # unwrap heading text
+        heading = hcontainer.find('a:heading', {'a': self.ns})
+        unwrap_element(heading)
+
 
 class CorrectAttachmentEids:
     """ Ensure that attachment eIds are correctly prefixed
     """
     def migrate_document(self, document):
-        if self.migrate_xml(document.doc.root, document.doc.namespace):
+        self.ns = document.doc.namespace
+        if self.migrate_xml(document.doc.root):
             return True
 
-    def migrate_xml(self, xml, ns):
+    def migrate_xml(self, xml):
         changed = False
         self.eid_mappings = {}
 
-        for att in xml.xpath('//a:attachment', namespaces={'a': ns}):
-            att_id = att.get('eId')
+        for att in xml.xpath('//a:attachment', namespaces={'a': self.ns}):
+            changed = changed or self.migrate_element(att)
 
-            for elem in att.xpath(f'.//a:*[@eId and not(starts-with(@eId, "{att_id}__"))]', namespaces={'a': ns}):
-                changed = True
-                new_id = f'{att_id}__{elem.attrib["eId"]}'
-                self.eid_mappings[elem.attrib['eId']] = elem.attrib['eId'] = new_id
+        return changed
+
+    def migrate_element(self, att):
+        att_id = att.get('eId')
+        changed = False
+
+        for elem in att.xpath(f'.//a:*[@eId and not(starts-with(@eId, "{att_id}__"))]', namespaces={'a': self.ns}):
+            changed = True
+            new_id = f'{att_id}__{elem.attrib["eId"]}'
+            self.eid_mappings[elem.attrib['eId']] = elem.attrib['eId'] = new_id
 
         return changed

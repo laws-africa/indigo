cf6605d6fb2ad603a9a0d92597d9530c2e5a7ac0
Musango <musangow@gmail.com>
2021-11-25 16:00:18 +0200

    components/index.js: remove toc-controller

--- indigo_app/js/components/index.js
+++ indigo_app/js/components/index.js
@@ -1,3 +1,2 @@
 export { default as Gutter } from './gutter/Gutter.vue';
-export { default as TOCController } from './toc-controller/index.vue';
 export { default as DocumentTOCView } from './DocumentTOCView.vue';

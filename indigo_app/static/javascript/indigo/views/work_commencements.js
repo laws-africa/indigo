9870549b946e00770ce6f5571b05c8799ac63267
Musango <musangow@gmail.com>
2022-02-10 16:27:52 +0200

    work_commencements. refactor check for provisions to use some

--- indigo_app/static/javascript/indigo/views/work_commencements.js
+++ indigo_app/static/javascript/indigo/views/work_commencements.js
@@ -84,7 +84,7 @@
       // if we've selected all provisions, ensure we don't send back any selections
       if (form.elements.all_provisions.checked) {
         // we can't iterate over this in-place, so do this instead
-        while (form.elements.provisions.some(provision => provision.checked)) {
+        while ([...form.elements.provisions].some(provision => provision.checked)) {
           form.elements.provisions[0].checked = false;
         }
       }

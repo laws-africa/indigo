c71604a07de03ff68a49403f62340c7421e6963e
goose-life <32566441+goose-life@users.noreply.github.com>
2022-03-10 12:40:21 +0200

    decode "unicode_escape' if decoding 'utf-8' doesn't work

--- indigo_api/importers/pdfs.py
+++ indigo_api/importers/pdfs.py
@@ -9,7 +9,10 @@ def pdf_count_pages(fname):
     """ Counts the number of pages in a PDF.
     """
     result = subprocess.run(["pdfinfo", fname], stdout=subprocess.PIPE, check=True)
-    m = re.search(r'^Pages:\s*(\d+)', result.stdout.decode('utf-8'), re.MULTILINE)
+    try:
+        m = re.search(r'^Pages:\s*(\d+)', result.stdout.decode('utf-8'), re.MULTILINE)
+    except UnicodeError:
+        m = re.search(r'^Pages:\s*(\d+)', result.stdout.decode('unicode_escape'), re.MULTILINE)
     if m:
         return int(m.group(1))
     else:
@@ -20,7 +23,10 @@ def pdf_is_encrypted(fname):
     """ Is this pdf encrypted?
     """
     result = subprocess.run(["pdfinfo", fname], stdout=subprocess.PIPE, check=True)
-    m = re.search(r'^Encrypted:\s*(\w+)', result.stdout.decode('utf-8'), re.MULTILINE)
+    try:
+        m = re.search(r'^Encrypted:\s*(\w+)', result.stdout.decode('utf-8'), re.MULTILINE)
+    except UnicodeError:
+        m = re.search(r'^Encrypted:\s*(\w+)', result.stdout.decode('unicode_escape'), re.MULTILINE)
     if m:
         return m.group(1).lower() == 'yes'
     else:

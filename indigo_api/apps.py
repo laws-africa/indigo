a702073c31ed926c763b73c3f1025c298da62d6d
Greg Kempe <greg@kempe.net>
2022-03-18 16:57:46 +0200

    Set cobalt source globally

--- indigo_api/apps.py
+++ indigo_api/apps.py
@@ -1,5 +1,9 @@
+import re
+
 from django.apps import AppConfig
 from django.conf import settings
+
+from cobalt import AkomaNtosoDocument
 
 
 class IndigoApiConfig(AppConfig):
@@ -19,6 +23,10 @@ class IndigoApiConfig(AppConfig):
         registry.register(PlaceSettings)
         registry.register(ArbitraryExpressionDate)
         registry.register(Commencement)
+
+        # set the global source for all Cobalt documents
+        id_ = re.sub(r'[^a-zA-Z0-9]', '-', settings.INDIGO_ORGANISATION)
+        AkomaNtosoDocument.source = [settings.INDIGO_ORGANISATION, id_, settings.INDIGO_URL]
 
         if not settings.DEBUG:
             from indigo_api.tasks import setup_pruning

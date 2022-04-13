3ef3193afd804e335f0f360279c3c079dea4e7c7
Greg Kempe <greg@kempe.net>
2022-04-13 10:09:07 +0200

    Underline in tables

--- indigo_app/static/ckeditor/config.js
+++ indigo_app/static/ckeditor/config.js
@@ -27,7 +27,7 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The default plugins included in the basic setup define some buttons that
 	// are not needed in a basic editor. They are removed here.
-	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Underline,Strike,Subscript,Superscript';
+	config.removeButtons = 'Cut,Copy,Paste,Undo,Redo,Anchor,Strike,Subscript,Superscript';
 
 	// Dialog windows are also simplified.
 	config.removeDialogTabs = 'link:advanced';

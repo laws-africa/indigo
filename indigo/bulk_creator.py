4b7943ef2830756f77021201d84784a98d4f4df4
goose-life <32566441+goose-life@users.noreply.github.com>
2022-05-23 10:50:32 +0200

    lowercase doctype choices

--- indigo/bulk_creator.py
+++ indigo/bulk_creator.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import re
 import csv
 import io
@@ -81,7 +80,7 @@ class RowValidationFormBase(forms.Form):
         self.fields['subtype'].choices = [(s.abbreviation, s.name) for s in subtypes]
 
     def get_doctypes_for_country(self, country_code):
-        return [[d[1], d[0]] for d in
+        return [[d[1].lower(), d[0]] for d in
                 settings.INDIGO['DOCTYPES'] +
                 settings.INDIGO['EXTRA_DOCTYPES'].get(country_code, [])]
 

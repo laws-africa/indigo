58ccf0016eec857464f62fcf7a0883499c0d073d
Greg Kempe <greg@kempe.net>
2022-06-20 13:50:18 +0200

    Update indigo/pipelines/text.py

--- indigo/pipelines/text.py
+++ indigo/pipelines/text.py
@@ -1,7 +1,7 @@
 import re
 import tempfile
 
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from lxml import etree
 from cobalt.akn import AkomaNtosoDocument
 

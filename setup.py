a598e0d5ff4d241ec638b513fbe4b34133470758
Greg Kempe <greg@kempe.net>
2022-07-11 20:20:18 +0200

    remove unused dependency on django-taggit

--- setup.py
+++ setup.py
@@ -63,8 +63,6 @@ setup(
         'django-sass-processor>=1.1',
         'django-sass-processor-dart-sass>=0.0.1',
         'django-storages>=1.12.3',
-        'django-taggit-serializer>=0.1.5',
-        'django-taggit>=1.2.0,<2.0.0',
         'django-templated-email>=2.3.0',
         'django-wkhtmltopdf>=3.3.0',
         'djangorestframework-xml>=1.3.0',

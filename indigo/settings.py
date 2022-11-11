f3b203e4912ad5a55f57bb1960d58ff41e6558fc
Eben van Deventer <buff0k@gmail.com>
2022-11-11 14:51:11 +0200

    Add TLS/SSL Email Support
    Add support for enabling and disabling either SSL or TLS through os.environ.get and DJANGO_EMAIL_USE_SSL=true or DJANGO_EMAIL_USE_TLS env variables.

--- indigo/settings.py
+++ indigo/settings.py
@@ -358,6 +358,8 @@ EMAIL_HOST = os.environ.get('DJANGO_EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD')
 EMAIL_PORT = int(os.environ.get('DJANGO_EMAIL_PORT', 25))
+EMAIL_USE_TLS = os.environ.get('DJANGO_EMAIL_USE_TLS', 'false') == 'true'
+EMAIL_USE_SSL = os.environ.get('DJANGO_EMAIL_USE_SSL', 'false') == 'true'
 EMAIL_SUBJECT_PREFIX = '[%s] ' % INDIGO_ORGANISATION
 
 # Auth

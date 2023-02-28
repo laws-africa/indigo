a4373d54c9d8f1954b33eebd10a0f24fa8334cd5
goose-life <32566441+goose-life@users.noreply.github.com>
2023-02-28 14:14:08 +0200

    add is_consolidation, uses_chapter to place settings form

--- indigo_app/forms.py
+++ indigo_app/forms.py
@@ -617,7 +617,8 @@ class NewCommencementForm(forms.ModelForm):
 class PlaceSettingsForm(forms.ModelForm):
     class Meta:
         model = PlaceSettings
-        fields = ('spreadsheet_url', 'as_at_date', 'styleguide_url', 'no_publication_document_text', 'consolidation_note')
+        fields = ('spreadsheet_url', 'as_at_date', 'styleguide_url', 'no_publication_document_text',
+                  'consolidation_note', 'is_consolidation', 'uses_chapter')
 
     spreadsheet_url = forms.URLField(required=False, validators=[
         URLValidator(

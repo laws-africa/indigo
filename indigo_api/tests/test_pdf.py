b0fb75ed1f35b9668c8f907f7a7c453269cbd8a6
goose-life <32566441+goose-life@users.noreply.github.com>
2023-04-24 12:55:43 +0200

    add passing test (basic)

---
+++ indigo_api/tests/test_pdf.py
@@ -0,0 +1,55 @@
+import os
+import tempfile
+from lxml import etree
+
+from django.test import TestCase
+
+from indigo_api.exporters import PDFExporter
+from indigo_api.pdf import run_fop
+from indigo_api.models import Document, Work, Language
+
+
+class PDFExporterTestCase(TestCase):
+    fixtures = ['languages_data', 'countries', 'user', 'taxonomies', 'work']
+    maxDiff = None
+    # show diffs, no matter how big
+    _diffThreshold = 999999999
+
+    def setUp(self):
+        self.exporter = PDFExporter()
+        self.maxDiff = None
+        self.eng = Language.objects.get(language__pk='en')
+        self.work = Work.objects.get(frbr_uri='/akn/za/act/1998/2')
+        self.document = Document(title="test", language=self.eng, work=self.work)
+        self.xsl_fo_path = os.path.join(os.path.dirname(__file__), '..', 'static', 'xsl', 'fo', 'fo_akn.xsl')
+
+    def run_and_compare(self, name, update=False):
+        input_path = os.path.join(os.path.dirname(__file__), 'pdf_fixtures', f'{name}_in.xml')
+        output_path = os.path.join(os.path.dirname(__file__), 'pdf_fixtures', f'{name}_out.xml')
+
+        with open(input_path, 'r') as f:
+            xml_in = f.read()
+
+        # run fop
+        with tempfile.TemporaryDirectory() as tmpdir:
+            xml_file = os.path.join(tmpdir, 'raw.xml')
+            with open(xml_file, "wb") as f:
+                f.write(xml_in.encode('utf-8'))
+            outf = tempfile.NamedTemporaryFile('rb', dir=tmpdir, suffix='.xml')
+            xsl_fo = self.xsl_fo_path
+            self.exporter.update_base_xsl_fo_dir(xsl_fo)
+            run_fop(outf.name, tmpdir, xml_file, xsl_fo, output_fo=True)
+            pretty_xml_out = etree.tostring(etree.fromstring(outf.read()), pretty_print=True, encoding='unicode')
+
+            if update:
+                # update the fixture to match the actual
+                with open(output_path, 'w') as ff:
+                    ff.write(pretty_xml_out)
+
+        with open(output_path, 'r') as f:
+            expected = f.read()
+
+        self.assertMultiLineEqual(expected, pretty_xml_out)
+
+    def test_basic(self):
+        self.run_and_compare('basic')

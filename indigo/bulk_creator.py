fd35570c9065cb7318fde7b46ce2c7a2e1f1cdf4
goose-life <32566441+goose-life@users.noreply.github.com>
2023-05-30 12:02:33 +0200

    add new update_commencement_date_main to BaseBulkUpdater

--- indigo/bulk_creator.py
+++ indigo/bulk_creator.py
@@ -1271,6 +1271,9 @@ class BaseBulkUpdater(BaseBulkCreator):
             # TODO: update extra properties
             # TODO: update taxonomies?
             for column in self.update_columns:
+                if column == 'commencement_date':
+                    update = self.update_commencement_date_main(row, work)
+                    continue
                 val = getattr(row, column)
                 old_val = getattr(work, column)
                 if old_val != val:
@@ -1317,6 +1320,43 @@ class BaseBulkUpdater(BaseBulkCreator):
         except Work.DoesNotExist:
             row.errors = f'Work not found for FRBR URI: {frbr_uri}'
             return row
+
+    def update_commencement_date_main(self, row, work):
+        val = getattr(row, 'commencement_date')
+        old_val = work.commencement_date
+        if old_val != val:
+            row.notes.append(f'commencement date (main): {old_val} → {val}')
+
+            if not self.dry_run:
+                main_commencement = self.get_or_create_commencement(work, val)
+                main_commencement.save()
+
+                # update work to 'commenced' or 'uncommenced' if needed
+                if val and not work.commenced:
+                    work.commenced = True
+                elif not val and work.commenced:
+                    work.commenced = False
+
+            return True
+
+    def get_or_create_commencement(self, work, new_date):
+        """ Looks for an existing main commencement on the work.
+            If it doesn't exist, create a new main commencement on it.
+            If there are no other commencements, have it commence all provisions (the common case).
+            Set the new date and return it without saving.
+        """
+        main_commencement = work.main_commencement
+        if not main_commencement:
+            main_commencement = Commencement(commenced_work=work, main=True, created_by_user=self.user)
+
+            other_commencements = work.commencements.all()
+            if not other_commencements:
+                main_commencement.all_provisions = True
+
+        main_commencement.date = new_date
+        main_commencement.updated_by_user = self.user
+
+        return main_commencement
 
     def get_row_validation_form(self, country, locality, subtypes, default_doctype, row_data):
         return self.row_validation_form_class(country, locality, subtypes, default_doctype, data=row_data,

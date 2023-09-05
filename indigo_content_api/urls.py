b24cb91662f34fcb8a274c3f782d2cb7f91ba866
goose-life <32566441+goose-life@users.noreply.github.com>
2023-09-05 10:44:46 +0200

    add v3 path to indigo_content_api urls; TODO for unversioned

--- indigo_content_api/urls.py
+++ indigo_content_api/urls.py
@@ -8,9 +8,11 @@ if settings.INDIGO_CONTENT_API_VERSIONED:
     # Versioned API URLs
     urlpatterns = [
         path('v2/', include(('indigo_content_api.v2.urls', APP_NAME), namespace='v2')),
+        path('v3/', include(('indigo_content_api.v3.urls', APP_NAME), namespace='v3')),
     ]
 else:
     # Unversioned API URLs, latest only
     urlpatterns = [
-        path('', include(('indigo_content_api.urls_v2', APP_NAME), namespace='v2'))
+        # TODO: where is urls_v2 currently?
+        path('', include(('indigo_content_api.urls_v2', APP_NAME), namespace='v3'))
     ]

65f95378bb9e52ed15c46658bec8fa7f8cd3fcae
goose-life <32566441+goose-life@users.noreply.github.com>
2023-09-06 14:42:17 +0200

    update v3 test_taxonomies_api

--- indigo_content_api/tests/v3/test_taxonomies_api.py
+++ indigo_content_api/tests/v3/test_taxonomies_api.py
@@ -6,67 +6,20 @@ class TaxonomyTopicsAPIV3Test(TaxonomiesAPIV2Test):
 
     def test_taxonomies(self):
         response = self.client.get(self.api_path + '/taxonomies.json')
-        self.assertEqual(404, response.status_code)
+        self.assertEqual(200, response.status_code)
 
         response = self.client.get(self.api_path + '/taxonomy_topics.json')
         self.assertEqual(200, response.status_code)
 
         self.assertEqual([
             {
-                "name": "Third Party Taxonomy",
-                "slug": "third-party-taxonomy",
+                "name": "Laws.Africa Subject Areas",
+                "slug": "lawsafrica-subject-areas",
                 "children": [
-                    {
-                        "name": "Fun stuff",
-                        "slug": "third-party-taxonomy-fun-stuff",
-                        "children": [],
-                    },
-                    {
-                        "name": "Not so fun stuff",
-                        "slug": "third-party-taxonomy-not-so-fun-stuff",
-                        "children": [],
-                    },
-                ],
-            }, {
-                "name": "Laws.Africa Subject Taxonomy",
-                "slug": "lawsafrica-subjects",
-                "children": [
-                    {
-                        "name": "Estates, Trusts and Succession",
-                        "slug": "lawsafrica-subjects-estates-trusts-and-succession",
-                        "children": [],
-                    },
                     {
                         "name": "Money and Business",
                         "slug": "lawsafrica-subjects-money-and-business",
-                        "children": [
-                            {
-                                "name": "Banking",
-                                "slug": "lawsafrica-subjects-money-and-business-banking",
-                                "children": [],
-                            },
-                            {
-                                "name": "Finance",
-                                "slug": "lawsafrica-subjects-money-and-business-finance",
-                                "children": [],
-                            },
-                        ],
-                    },
-                    {
-                        "name": "People and Work",
-                        "slug": "lawsafrica-subjects-people-and-work",
-                        "children": [
-                            {
-                                "name": "Children",
-                                "slug": "lawsafrica-subjects-people-and-work-children",
-                                "children": [],
-                            },
-                            {
-                                "name": "Communications",
-                                "slug": "lawsafrica-subjects-people-and-work-communications",
-                                "children": [],
-                            },
-                        ],
+                        "children": [],
                     },
                 ],
             },

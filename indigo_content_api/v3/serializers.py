a3706d819e46df30cf331a07f863e7823f89182b
goose-life <32566441+goose-life@users.noreply.github.com>
2023-09-06 09:39:19 +0200

    update serializer

--- indigo_content_api/v3/serializers.py
+++ indigo_content_api/v3/serializers.py
@@ -7,26 +7,10 @@ class PublishedDocumentSerializerV3(PublishedDocumentSerializerV2):
     - Excludes detailed commencement information.
     - Excludes taxonomies.
     """
-    # TODO: is this right, or just change Meta.fields?
     commencements = None
     taxonomies = None
 
     class Meta:
-        # TODO: fine to have no model here (does it inherit from the base)?
-        # TODO: inherit super fields, exclude 'commencements' and 'taxonomies'?
-        fields = (
-            'url', 'title',
-            'created_at', 'updated_at',
-
-            # frbr_uri components
-            # year is for backwards compatibility
-            'country', 'locality', 'nature', 'subtype', 'date', 'year', 'actor', 'number', 'frbr_uri', 'expression_frbr_uri',
-
-            'publication_date', 'publication_name', 'publication_number', 'publication_document',
-            'expression_date', 'commenced', 'commencement_date', 'assent_date',
-            'language', 'repeal', 'amendments', 'work_amendments', 'points_in_time', 'parent_work', 'custom_properties',
-            'numbered_title', 'taxonomy_topics', 'as_at_date', 'stub', 'principal', 'type_name',
-
-            'links',
-        )
+        model = PublishedDocumentSerializerV2.Meta.model
+        fields = tuple(x for x in PublishedDocumentSerializerV2.Meta.fields if x not in ['commencements', 'taxonomies'])
         read_only_fields = fields

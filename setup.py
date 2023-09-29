6a2fe6b93f804ae0796b458b4a61c52696aaa9fd
edith <32566441+goose-life@users.noreply.github.com>
2023-09-29 12:38:07 +0200

    Update setup.py
    finalise docpipe commit

--- setup.py
+++ setup.py
@@ -68,8 +68,7 @@ setup(
         'djangorestframework-xml>=1.3.0',
         'djangorestframework>=3.11.0,<3.12.0',  # v3.12.0: The authtoken model no longer exposes
                                                 # the pk in the admin URL. [#7341]
-        # TODO: update this once the PR is merged
-        'docpipe @ git+https://github.com/laws-africa/docpipe@25a8dd853517fd9f2263744f26702256e72c5cea',
+        'docpipe @ git+https://github.com/laws-africa/docpipe@5dee72f4a188a31c9077c86982d9663a330d9e49',
         'EbookLib>=0.15',
         'google-api-python-client>=1.7.9',
         'iso8601>=0.1',

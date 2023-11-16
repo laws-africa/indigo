9f0606a101b2a37ade701afe4dfa24a18c5fad92
goose-life <32566441+goose-life@users.noreply.github.com>
2023-11-16 16:09:52 +0200

    fix when publication document is missing

--- indigo_app/views/places.py
+++ indigo_app/views/places.py
@@ -1128,7 +1128,7 @@ class WorkDetailView(PlaceViewBase, DetailView):
             OverviewDataEntry(key=_(prop["label"]), value=prop["value"]) for prop in work.labeled_properties()
         ]
 
-        publication = describe_publication_event(work, placeholder=bool(work.publication_document))
+        publication = describe_publication_event(work, placeholder=hasattr(work, 'publication_document'))
         if publication:
             overview_data.append(OverviewDataEntry(key=_("Publication"), value=_(publication.description)))
 

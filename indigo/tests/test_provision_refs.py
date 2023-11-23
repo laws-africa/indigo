596eecdf48d35da1434813fe6393eb3fc7c7577d
Greg Kempe <greg@kempe.net>
2023-11-23 17:13:31 +0200

    support hyphens / emdash for provision ref ranges

--- indigo/tests/test_provision_refs.py
+++ indigo/tests/test_provision_refs.py
@@ -1442,6 +1442,24 @@ class ProvisionRefsGrammarTest(TestCase):
         ], result.references)
         self.assertIsNone(result.target)
 
+    def test_range(self):
+        result = parse_provision_refs("Section 1.2(1) to (3), (ii) — (iii), (g) - (j)")
+        self.assertEqual([
+            MainProvisionRef(
+                'Section',
+                ProvisionRef('1.2', 8, 11, None,
+                             ProvisionRef('(1)', 11, 14)),
+                             [
+                                 ProvisionRef('(3)', 18, 21, 'range'),
+                                 ProvisionRef('(ii)', 23, 27, 'and_or'),
+                                 ProvisionRef('(iii)', 30, 35, 'range'),
+                                 ProvisionRef('(g)', 37, 40, 'and_or'),
+                                 ProvisionRef('(j)', 43, 46, 'range')
+                             ]
+            )
+        ], result.references)
+        self.assertIsNone(result.target)
+
     def test_mixed_af(self):
         result = parse_provision_refs("Afdeling 1.2(1)(a),(c) tot (e), (f)(ii) en (2), en (3)(g),(h) en afdelings 32(a)")
         self.assertEqual([

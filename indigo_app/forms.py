09f0dff29b4c9272da3f1986df39a541faa908b4
goose-life <32566441+goose-life@users.noreply.github.com>
2023-12-13 13:20:41 +0200

    merge main

--- indigo_app/forms.py
+++ indigo_app/forms.py
@@ -85,6 +85,7 @@ class WorkForm(forms.ModelForm):
             self.fields[key] = forms.CharField(label=label, required=False)
 
         if self.instance:
+            # self.fields['commencing_work'].initial = self.instance.commencing_work
             self.fields['frbr_doctype'].initial = self.instance.doctype
             self.fields['frbr_subtype'].initial = self.instance.subtype
             self.fields['frbr_date'].initial = self.instance.date

0728c94a418960fc621b4499269abe0f817e8c78
goose-life <32566441+goose-life@users.noreply.github.com>
2024-02-20 11:16:15 +0200

    drive tasks from the relevant amendments; group tasks under amended works

--- indigo_app/views/places.py
+++ indigo_app/views/places.py
@@ -1275,8 +1275,9 @@ class WorkBulkApproveView(PlaceViewBase, FormView):
         context["works_in_progress"] = works_in_progress = form.cleaned_data.get("works_in_progress").order_by("-created_at")
         context["import_task_works"] = works_in_progress.filter(principal=True)
         context["gazette_task_works"] = [w for w in works_in_progress if not w.has_publication_document()]
-        context["amendments_per_work"] = {w: w.amendments.all() for w in works_in_progress if w.amendments.exists()}
-        context["amendments_made_per_work"] = {w: w.amendments_made.all() for w in works_in_progress if w.amendments_made.exists()}
+        context["amendments"] = all_amendments = form.get_amendments()
+        amended_works = set(a.amended_work for a in all_amendments)
+        context["amendments_per_work"] = {w: w.amendments.filter(pk__in=[a.pk for a in all_amendments]) for w in amended_works}
         return context
 
     def form_valid(self, form):
@@ -1287,13 +1288,8 @@ class WorkBulkApproveView(PlaceViewBase, FormView):
                 messages.success(self.request, f"Created {form.cleaned_data['import_task_works'].count()} Import tasks.")
             if form.cleaned_data.get('gazette_task_works'):
                 messages.success(self.request, f"Created {form.cleaned_data['gazette_task_works'].count()} Gazette tasks.")
-            if form.cleaned_data.get('amendment_task_works'):
-                amendments_count = 0
-                for work in form.cleaned_data['amendment_task_works']:
-                    amendments_count += work.amendments.count()
-                for work in form.cleaned_data['amendment_made_task_works']:
-                    amendments_count += work.amendments_made.count()
-                messages.success(self.request, f"Created {amendments_count} Amendment tasks.")
+            if form.cleaned_data.get('amendments'):
+                messages.success(self.request, f"Created {form.cleaned_data['amendments'].count()} Amendment tasks.")
             return redirect(self.request.headers["Referer"])
         return self.form_invalid(form)
 

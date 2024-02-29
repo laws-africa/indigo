2bdf9c7cd7935f3d0dcd029977d31185709caeaa
goose-life <32566441+goose-life@users.noreply.github.com>
2024-02-29 10:47:33 +0200

    stash all the tasks on the broker

--- indigo/tasks.py
+++ indigo/tasks.py
@@ -11,11 +11,13 @@ class TaskBroker:
         self.import_task_works = works.filter(principal=True)
         self.import_tasks = None
         self.gazette_task_works = [w for w in works if not w.has_publication_document()]
+        self.gazette_tasks = None
         all_amendments = [w.amendments.all() for w in works if w.amendments.exists()] + \
                          [w.amendments_made.all() for w in works if w.amendments_made.exists()]
         self.amendments = set(chain(*all_amendments))
         self.amendments_per_work = {w: w.amendments.filter(pk__in=[a.pk for a in self.amendments])
                                     for w in set(a.amended_work for a in self.amendments)}
+        self.amendment_tasks = None
 
     def create_tasks(self, user, data):
         # import tasks
@@ -28,21 +30,23 @@ class TaskBroker:
             self.block_or_cancel_tasks(self.import_tasks, data['update_import_tasks'], user)
 
         # gazette tasks
-        gazette_tasks = [self.get_or_create_task(work=work, task_type='link-gazette',
-                                                 description=data['gazette_task_description'],
-                                                 user=user) for work in self.gazette_task_works]
+        # stash the gazette tasks in case we want to do something with them later
+        self.gazette_tasks = [self.get_or_create_task(work=work, task_type='link-gazette',
+                                                      description=data['gazette_task_description'],
+                                                      user=user) for work in self.gazette_task_works]
         if data.get('update_gazette_tasks'):
-            self.block_or_cancel_tasks(gazette_tasks, data['update_gazette_tasks'], user)
+            self.block_or_cancel_tasks(self.gazette_tasks, data['update_gazette_tasks'], user)
 
         # amendment tasks
-        amendment_tasks = []
+        # stash the amendment tasks in case we want to do something with them later
+        self.amendment_tasks = []
         for amendment in self.amendments:
-            amendment_tasks.append(self.get_or_create_task(
+            self.amendment_tasks.append(self.get_or_create_task(
                 work=amendment.amended_work, task_type='apply-amendment',
                 description=data[f'amendment_task_description_{amendment.pk}'],
                 user=user, timeline_date=amendment.date))
         if data.get('update_amendment_tasks'):
-            self.block_or_cancel_tasks(amendment_tasks, data['update_amendment_tasks'], user)
+            self.block_or_cancel_tasks(self.amendment_tasks, data['update_amendment_tasks'], user)
 
     def get_or_create_task(self, work, task_type, description, user, timeline_date=None):
         task = Task.objects.filter(work=work, code=task_type, timeline_date=timeline_date, state__in=Task.OPEN_STATES).first()

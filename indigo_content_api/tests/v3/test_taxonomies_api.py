fb42e3bf27ab086d14a6ddb4e9df3e8da4f2ed73
Greg Kempe <greg@kempe.net>
2024-03-15 14:18:42 +0200

    tests

--- indigo_content_api/tests/v3/test_taxonomies_api.py
+++ indigo_content_api/tests/v3/test_taxonomies_api.py
@@ -10,6 +10,7 @@ class TaxonomyTopicsAPIV3Test(TaxonomiesAPIV2Test):
         # /taxonomies isn't in v3
         self.assertEqual(404, response.status_code)
 
+    def test_taxonomy_topics_list(self):
         response = self.client.get(self.api_path + '/taxonomy-topics.json')
         self.assertEqual(200, response.status_code)
 
@@ -29,3 +30,38 @@ class TaxonomyTopicsAPIV3Test(TaxonomiesAPIV2Test):
                 ],
             },
         ], taxonomy_topics)
+
+    def test_taxonomy_topic_root_detail(self):
+        response = self.client.get(self.api_path + '/taxonomy-topics/lawsafrica-subject-areas.json')
+        self.assertEqual(200, response.status_code)
+
+        topic = response.json()
+        self.assertEqual({
+            "name": "Laws.Africa Subject Areas",
+            "slug": "lawsafrica-subject-areas",
+            "id": 1,
+            "children": [
+                {
+                    "name": "Money and Business",
+                    "slug": "lawsafrica-subject-areas-money-and-business",
+                    "id": 2,
+                    "children": [],
+                },
+            ],
+        }, topic)
+
+    def test_taxonomy_topic_child_detail(self):
+        response = self.client.get(self.api_path + '/taxonomy-topics/lawsafrica-subject-areas-money-and-business.json')
+        self.assertEqual(200, response.status_code)
+
+        topic = response.json()
+        self.assertEqual({
+            "name": "Money and Business",
+            "slug": "lawsafrica-subject-areas-money-and-business",
+            "id": 2,
+            "children": [],
+        }, topic)
+
+    def test_taxonomy_topic_detail_not_public(self):
+        self.assertEqual(404, self.client.get(self.api_path + '/taxonomy-topics/internal-topic.json').status_code)
+        self.assertEqual(404, self.client.get(self.api_path + '/taxonomy-topics/internal-topic-im-private.json').status_code)

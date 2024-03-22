b5c0988e9c9ed5c6f08622fb1d85fb67a9f029dd
Greg Kempe <greg@kempe.net>
2024-03-22 16:27:34 +0200

    some tests

--- indigo_app/tests/test_places.py
+++ indigo_app/tests/test_places.py
@@ -35,6 +35,20 @@ class PlacesTest(testcases.TestCase):
         response = self.client.get('/places/za/works')
         self.assertEqual(response.status_code, 200)
 
+        response = self.client.get('/places/za/works/facets')
+        self.assertEqual(response.status_code, 200)
+
+    def test_all_place_works(self):
+        response = self.client.get('/places/all/works')
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get('/places/all/works/facets')
+        self.assertEqual(response.status_code, 200)
+
+    def test_all_place_tasks_404(self):
+        response = self.client.get('/places/all/tasks')
+        self.assertEqual(response.status_code, 404)
+
     def test_place_works_xlsx(self):
         response = self.client.get('/places/za/works?format=xlsx')
         self.assertEqual(response.status_code, 200)

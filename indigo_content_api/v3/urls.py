8131fe6f61abfa01d8e2c04f2e100f8ae2261b32
Greg Kempe <greg@kempe.net>
2024-04-11 16:30:45 +0200

    use drf for routes

--- indigo_content_api/v3/urls.py
+++ indigo_content_api/v3/urls.py
@@ -9,17 +9,11 @@ from indigo_content_api.v3.views import PublishedDocumentDetailViewV3, TaxonomyT
 router = DefaultRouter(trailing_slash=False)
 router.register('countries', CountryViewSet, basename='country')
 router.register('taxonomy-topics', TaxonomyTopicView, basename='taxonomy_topic')
+router.register(r'taxonomy-topics/(?P<slug>[^/.]+)/work-expressions', TaxonomyTopicPublishedDocumentsView, basename='taxonomy_topic-work-expressions')
 router.register('work-expressions', WorkExpressionsViewSet, basename='work_expression')
-
 
 urlpatterns = urlpatterns_base + [
     path('', include(router.urls)),
-
     # eg. /akn/za/act/2007/98
     re_path(r'^(?P<frbr_uri>akn/[a-z]{2}[-/].*)$', PublishedDocumentDetailViewV3.as_view({'get': 'get'}), name='published-document-detail'),
-
-    # taxonomy topic work list
-    path('taxonomy-topics/<slug:slug>/work-expressions', TaxonomyTopicPublishedDocumentsView.as_view({'get': 'list'}), name='taxonomy_topic-work-expressions'),
-    path('taxonomy-topics/<slug:slug>/work-expressions.<slug:format>', TaxonomyTopicPublishedDocumentsView.as_view({'get': 'list'}),
-         name='taxonomy_topic-work-expressions'),
 ]

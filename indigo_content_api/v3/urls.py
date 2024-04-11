5b9e1064b8b9d7a7644750df53a6f6ac70769fe0
Greg Kempe <greg@kempe.net>
2024-04-11 15:31:50 +0200

    global work-expressions api endpoint

--- indigo_content_api/v3/urls.py
+++ indigo_content_api/v3/urls.py
@@ -3,15 +3,17 @@ from rest_framework.routers import DefaultRouter
 
 from indigo_content_api.v2.urls import urlpatterns_base
 from indigo_content_api.v2.views import CountryViewSet, TaxonomyTopicView
-from indigo_content_api.v3.views import PublishedDocumentDetailViewV3, TaxonomyTopicPublishedDocumentsView
+from indigo_content_api.v3.views import PublishedDocumentDetailViewV3, TaxonomyTopicPublishedDocumentsView, \
+    WorkExpressionsViewSet
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'countries', CountryViewSet, basename='country')
-router.register(r'taxonomy-topics', TaxonomyTopicView, basename='taxonomy_topic')
+router.register('countries', CountryViewSet, basename='country')
+router.register('taxonomy-topics', TaxonomyTopicView, basename='taxonomy_topic')
+router.register('work-expressions', WorkExpressionsViewSet, basename='work_expression')
 
 
 urlpatterns = urlpatterns_base + [
-    path(r'', include(router.urls)),
+    path('', include(router.urls)),
 
     # eg. /akn/za/act/2007/98
     re_path(r'^(?P<frbr_uri>akn/[a-z]{2}[-/].*)$', PublishedDocumentDetailViewV3.as_view({'get': 'get'}), name='published-document-detail'),

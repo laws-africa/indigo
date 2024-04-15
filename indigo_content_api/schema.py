50fb585649602c8e7ecafa0e3ce78101d1aaab94
Greg Kempe <greg@kempe.net>
2024-04-15 15:22:20 +0200

    comment

--- indigo_content_api/schema.py
+++ indigo_content_api/schema.py
@@ -1,6 +1,7 @@
 def preprocess(endpoints):
     for i, (path, path_regex, method, callback) in enumerate(endpoints):
         if path == '/{frbr_uri}':
+            # adjust this endpoint so that drf-spectacular documents the LIST api, not the RETRIEVE api
             endpoints[i] = (path, path_regex, method, callback.cls.as_view({'get': 'list'}))
             break
     return endpoints

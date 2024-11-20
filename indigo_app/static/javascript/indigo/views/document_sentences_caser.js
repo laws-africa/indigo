3cbddda381cb54a2e3433998e50b38d6da0bbcc2
goose-life <32566441+goose-life@users.noreply.github.com>
2024-11-20 09:25:44 +0200

    remove unused javascript, other minor tweaks

--- indigo_app/static/javascript/indigo/views/document_sentences_caser.js
+++ indigo_app/static/javascript/indigo/views/document_sentences_caser.js
@@ -14,15 +14,10 @@
     },
 
     sentenceCaseHeadings: function(e) {
-      var self = this,
-          $btn = this.$el.find('.sentence-case-headings'),
+      let self = this,
           data = {'document': this.model.document.toJSON()};
 
       data.document.content = this.model.toXml();
-
-      $btn
-        .prop('disabled', true)
-        .find('i').addClass('fa-spin');
 
       $.ajax({
         url: this.model.document.url() + '/analysis/sentence-case-headings',
@@ -30,14 +25,9 @@
         data: JSON.stringify(data),
         contentType: "application/json; charset=utf-8",
         dataType: "json"})
-        .then(function(response) {
-          self.model.set('content', response.document.content);
-        })
-        .always(function() {
-          $btn
-            .prop('disabled', false)
-            .find('i').removeClass('fa-spin');
-        });
+          .then(function(response) {
+            self.model.set('content', response.document.content);
+          });
     },
   });
 })(window);

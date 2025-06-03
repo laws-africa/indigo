6d33aab056077b4663edb7e5be0a4f5566943d4e
goose-life <32566441+goose-life@users.noreply.github.com>
2025-06-03 21:59:29 +0200

    update work_chapter_number() to deal with more scenarios

--- indigo/tests/test_work_detail.py
+++ indigo/tests/test_work_detail.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils.translation import override
 
 from indigo.analysis.work_detail import BaseWorkDetail
-from indigo_api.models import Work, Country
+from indigo_api.models import Work, Country, ChapterNumber
 
 
 class BaseWorkDetailTestCase(TestCase):
@@ -21,7 +21,6 @@ class BaseWorkDetailTestCase(TestCase):
         self.assertEqual('Act 32 of 1999', self.plugin.work_numbered_title(work))
         with override('fr', deactivate=True):
             self.assertEqual('Loi 32 de 1999', self.plugin.work_numbered_title(work))
-
 
     def test_numbered_title_caps(self):
         work = Work.objects.create(
@@ -63,3 +62,72 @@ class BaseWorkDetailTestCase(TestCase):
             frbr_uri="/akn/za/bill/1999/32"
         )
         self.assertEqual('Bill 32 of 1999', self.plugin.work_numbered_title(work))
+
+    def test_numbered_title_chapter_simple(self):
+        work = Work.objects.create(
+            country=self.country,
+            title="test",
+            frbr_uri="/akn/za/act/1999/32"
+        )
+        ChapterNumber.objects.create(number='123', work=work)
+        self.assertEqual('Chapter 123', self.plugin.work_numbered_title(work))
+        # but adding a second one without a date gives us nothing to work with
+        ChapterNumber.objects.create(number='456', work=work)
+        self.assertEqual('Act 32 of 1999', self.plugin.work_numbered_title(work))
+
+    def test_numbered_title_chapter_two_same_start_date(self):
+        work = Work.objects.create(
+            country=self.country,
+            title="test",
+            frbr_uri="/akn/za/act/1999/32"
+        )
+        ChapterNumber.objects.create(number='123', work=work, validity_start_date='2025-06-03')
+        self.assertEqual('Chapter 123', self.plugin.work_numbered_title(work))
+        # but adding a second one at the same date gives us nothing to work with
+        ChapterNumber.objects.create(number='456', work=work, validity_start_date='2025-06-03')
+        self.assertEqual('Act 32 of 1999', self.plugin.work_numbered_title(work))
+
+    def test_numbered_title_chapter_different_start_dates(self):
+        work = Work.objects.create(
+            country=self.country,
+            title="test",
+            frbr_uri="/akn/za/act/1999/32"
+        )
+        ChapterNumber.objects.create(number='123', work=work, validity_start_date='2025-06-03')
+        self.assertEqual('Chapter 123', self.plugin.work_numbered_title(work))
+        ChapterNumber.objects.create(number='456', work=work, validity_start_date='2025-06-04')
+        self.assertEqual('Chapter 456', self.plugin.work_numbered_title(work))
+        # but adding a third one without a date gives us nothing to work with
+        ChapterNumber.objects.create(number='789', work=work)
+        self.assertEqual('Act 32 of 1999', self.plugin.work_numbered_title(work))
+
+    def test_numbered_title_chapter_mix_start_and_end_dates_1(self):
+        work = Work.objects.create(
+            country=self.country,
+            title="test",
+            frbr_uri="/akn/za/act/1999/32"
+        )
+        ChapterNumber.objects.create(number='123', work=work, validity_start_date='2025-06-03')
+        self.assertEqual('Chapter 123', self.plugin.work_numbered_title(work))
+        ChapterNumber.objects.create(number='456', work=work, validity_end_date='2025-06-02')
+        self.assertEqual('Chapter 123', self.plugin.work_numbered_title(work))
+        # another one at an older start date
+        ChapterNumber.objects.create(number='789', work=work, validity_start_date='2025-06-02')
+        self.assertEqual('Chapter 123', self.plugin.work_numbered_title(work))
+        # another one at the same start date -- back to numbered title
+        ChapterNumber.objects.create(number='789', work=work, validity_start_date='2025-06-03')
+        self.assertEqual('Act 32 of 1999', self.plugin.work_numbered_title(work))
+
+    def test_numbered_title_chapter_mix_start_and_end_dates_2(self):
+        work = Work.objects.create(
+            country=self.country,
+            title="test",
+            frbr_uri="/akn/za/act/1999/32"
+        )
+        ChapterNumber.objects.create(number='123', work=work, validity_start_date='2025-06-03')
+        self.assertEqual('Chapter 123', self.plugin.work_numbered_title(work))
+        ChapterNumber.objects.create(number='456', work=work, validity_end_date='2025-06-02')
+        self.assertEqual('Chapter 123', self.plugin.work_numbered_title(work))
+        # another one at a more recent end date
+        ChapterNumber.objects.create(number='789', work=work, validity_end_date='2025-06-03')
+        self.assertEqual('Act 32 of 1999', self.plugin.work_numbered_title(work))
